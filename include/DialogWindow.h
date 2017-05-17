8f832cd8931a0cf2a674023b65fbd34fc9869af8
gerudoking <edsantos2003@gmail.com>
2017-05-17 14:46:28 -0300

    Texto inserido na DialogWindow

--- include/DialogWindow.h
+++ include/DialogWindow.h
@@ -4,16 +4,18 @@
 #include "Window.h"
 #include "Sprite.h"
 #include "Button.h"
+#include "Text.h"
 #include <string>
 
 class DialogWindow: public Window{
 public:
-	DialogWindow(int posX, int posY, int w, int h);
+	DialogWindow(int posX, int posY, int w, int h, std::string texto);
 	void Update(float dt);
 	void Render(int cameraX = 0, int cameraY = 0);
 
 private:
 	void RenderPiece(int posx, int posy, int x, int y, int w, int h);
+	Text dialog;
 	Button okay;
 };
 
--- src/DialogWindow.cpp
+++ src/DialogWindow.cpp
@@ -1,47 +1,21 @@
 #include "DialogWindow.h"
 
-DialogWindow::DialogWindow(int posX, int posY, int w, int h){
+DialogWindow::DialogWindow(int posX, int posY, int w, int h, std::string texto){
 	sp = Sprite("window.png");
 
 	box.x = posX;
 	box.y = posY;
 	box.w = w;
 	box.h = h;
+
+	dialog = Text("Raleway-Light.ttf", 12, Text::BLENDED, SDL_Color {255, 255, 255}, texto, box.x, box.y);
 }
 
 void DialogWindow::Update(float dt){
 
 }
 
-/*void DialogWindow::RenderPiece(int posx, int posy, int x, int y, int w, int h){
-	sp.SetClip(x, y, w, h);
-	sp.Render(posx, posy);
-}
-
 void DialogWindow::Render(int cameraX, int cameraY){
-	int sizex = (box.w - box.x)/WINDOWPIECE;
-	int sizey = (box.h - box.y)/WINDOWPIECE;
-
-	for(int i = 0; i < sizex; i++){
-		for(int j = 0; j < sizey; j++){
-			if(i == 0 && j == 0)
-				RenderPiece(box.x + i*WINDOWPIECE - cameraX, box.y + j*WINDOWPIECE - cameraY, 0, 0, 32, 32);
-			else if(i == 0 && j == sizey - 1)
-				RenderPiece(box.x + i*WINDOWPIECE - cameraX, box.y + j*WINDOWPIECE - cameraY, 0, 32, 32, 64);
-			else if(i == sizex - 1 && j == 0)
-				RenderPiece(box.x + i*WINDOWPIECE - cameraX, box.y + j*WINDOWPIECE - cameraY, 32, 0, 64, 32);
-			else if(i == sizex - 1 && j == sizey - 1)
-				RenderPiece(box.x + i*WINDOWPIECE - cameraX, box.y + j*WINDOWPIECE - cameraY, 32, 32, 64, 64);
-			else if(i == 0 && j < sizey - 1)
-				RenderPiece(box.x + i*WINDOWPIECE - cameraX, box.y + j*WINDOWPIECE - cameraY, 0, 16, 32, 48);
-			else if(i < sizex - 1 && j == 0)
-				RenderPiece(box.x + i*WINDOWPIECE - cameraX, box.y + j*WINDOWPIECE - cameraY, 16, 0, 48, 32);
-			else if(i == sizex - 1 && j < sizey - 1)
-				RenderPiece(box.x + i*WINDOWPIECE - cameraX, box.y + j*WINDOWPIECE - cameraY, 32, 16, 64, 48);
-			else if(i < sizex - 1 && j == sizey - 1)
-				RenderPiece(box.x + i*WINDOWPIECE - cameraX, box.y + j*WINDOWPIECE - cameraY, 16, 32, 48, 64);
-			else
-				RenderPiece(box.x + i*WINDOWPIECE - cameraX, box.y + j*WINDOWPIECE - cameraY, 16, 16, 48, 48);
-		}
-	}
-}*/
+	Window::Render();
+	dialog.Render(cameraX, cameraY);
+}
