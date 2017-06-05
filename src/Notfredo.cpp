2002b5a28ffcf305bc9960c01eb6e35e76c74e27
Luiz <luiz.duarte11@yahoo.com.br>
2017-06-05 00:12:24 -0300

    Começo do pathfinding

--- src/Notfredo.cpp
+++ src/Notfredo.cpp
@@ -10,6 +10,18 @@
 #include "Projectile.h"
 #include "IAComponent.h"
 
+typedef struct Node node;
+
+struct Node{
+  int x,y,z;
+  float graph_distance;
+  float physical_distance;
+  float combined_distance;
+  bool discovered;
+  node* going_through;
+  node* next;
+};
+
 Notfredo::Notfredo(int x, int y):
 	Character(x,y),
 	radius(),
@@ -63,6 +75,70 @@ void Notfredo::UpdateTimers(float dt)
 		if(!idle.IsRunning())
 			looking.Start();
 	}
+}
+
+node* Notfredo::New(int x,int y,int z){
+    node* aux = new node;
+    aux->x = x;
+    aux->y = y;
+    aux->z = z;
+    aux->combined_distance = MAX_VALUE;
+    aux->discovered = false;
+    aux->going_through = nullptr;
+    aux->graph_distance = 0;
+    aux->next = nullptr;
+    aux->physical_distance = 0;
+
+    return aux;
+}
+
+
+node* Notfredo::Pop(node* stack){
+    node* aux = stack;
+
+    stack = stack->next;
+
+    return aux;
+}
+
+void Notfredo::Push(int x,int y,int z,node* stack){
+    node* aux = stack;
+
+    while(aux->next != nullptr){
+        aux = aux->next;
+    }
+
+    aux->next = New(x,y,z);
+}
+
+void Notfredo::PathFind(){
+
+    node tileStack;
+    //for all tiles in current tilemap
+        //push new node with tile coordinates,
+        //combined distance of MAX_VALUE(to be determined),
+        //and physical_distance as the distance between the
+        //tile and the target(player)
+
+    //starting with start tile(fredo's tile)
+    //while top of stack is not the target's tile
+        //pop top of stack. That is the current node
+
+        //for all nodes adjacent to current node
+            //if node is undiscovered(discovered == false)
+                //tile's graph distance is current node's graph distance plus distance
+                //between current node and adjacent node
+
+                //tile's combined distance is it's graph distance plus its physical distance
+
+            //else if tile's graph distance is lower than current tile's graph distance plus
+            //distance between current node and adjacent node
+                //replace graph distance in tile and update combined distance
+                //else, do nothing
+        //order stack by total distance
+
+    //return sequence of tiles to target
+
 }
 
 void Notfredo::UpdateAI(float dt)
