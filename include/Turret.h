3fdbdd48cb7cdaa7e436e24003a7662c17eeab2f
Filipe Caixeta <filipecaixeta@gmail.com>
2017-07-07 00:05:38 -0300

    ai do lixo

--- include/Turret.h
+++ include/Turret.h
@@ -7,8 +7,18 @@ class Turret : public Character
 {
 public:
 	Turret(int x, int y);
-	void Update(TileMap* map,float dt);
-	void UpdateAI(TileMap* map,float dt);
+	~Turret();
+	void Attack();
+	void NotifyTileCollision(int tile, Face face);
+	void UpdateTimers(float dt);
+	void UpdateAI(float dt);
+	void Update(TileMap* world, float dt);
+	bool Is(std::string type);
+
+private:
+	Rect radius;
+	Timer looking;
+	Timer idle;
 };
 
 #endif // TURRET_H
