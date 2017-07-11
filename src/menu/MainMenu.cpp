813e8aabdbc0a99c4e17cdfbefe1a5461bca234f
Filipe Caixeta <filipecaixeta@gmail.com>
2017-07-11 00:09:10 -0300

    doc

--- src/menu/MainMenu.cpp
+++ src/menu/MainMenu.cpp
@@ -22,8 +22,7 @@ void MainMenu::LoadAssets()
 {
 	SDL_Texture *text;
 
-	menuOptions.push_back({"Galahad",new Sprite("menus/new-game-button.png"),true,0});
-	menuOptions.push_back({"Lancelot",new Sprite("menus/new-game-button.png"),true,0});
+	menuOptions.push_back({"NewGame",new Sprite("menus/new-game-button.png"),true,0});
 	menuOptions.push_back({"Load",new Sprite("menus/load-game-button.png"),true,0});
 	menuOptions.push_back({"Settings",new Sprite("menus/settings-button.png"),true,0});
 	menuOptions.push_back({"Exit",new Sprite("menus/exit-button.png"),true,0});
@@ -44,12 +43,8 @@ void MainMenu::Update()
 	if(	InputManager::GetInstance().KeyPress(SDLK_KP_ENTER) ||
 		InputManager::GetInstance().KeyPress(SDLK_RETURN) )
 	{
-		if (SelectedOptionIs("Lancelot"))
-		{
-			music.Stop();
-			Game::GetInstance().AddState(new Cutscene(0, true));
-		}
-		if (SelectedOptionIs("Galahad"))
+
+		if (SelectedOptionIs("NewGame"))
 		{
 			music.Stop();
 			Game::GetInstance().AddState(new StageState("cidadeGalahad"));
@@ -71,7 +66,7 @@ void MainMenu::Update()
 
 void MainMenu::Render()
 {
-	int positionsY[5] = {370,431,498,564,631};
+	int positionsY[4] = {431,498,564,631};
 	bg.Render(CenterVertical(&bg));
 	bgOptions.Render(CenterVertical(&bgOptions)+Vec2(0,431));
 	menuOptions[currentOption].sprite->Render(CenterVertical(menuOptions[currentOption].sprite)+Vec2(0,positionsY[currentOption]));
