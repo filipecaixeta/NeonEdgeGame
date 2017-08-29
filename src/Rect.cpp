eb824d21b6db6c31f844d5387df3401c4d4514d6
lbruno55 <l_brunofidelis@yahoo.com.br>
2017-08-29 00:08:54 -0300

    Aplicação 2 da folha de estilo em Rect.cpp e Turret.cpp

--- src/Rect.cpp
+++ src/Rect.cpp
@@ -1,87 +1,75 @@
+// Copyright 2017 Neon Edge Game.
+
 #include "Rect.h"
 
-Rect::Rect()
-{
-	x = 0;
-	y = 0;
-	w = 0;
-	h = 0;
+Rect::Rect() {
+    x = 0;
+    y = 0;
+    w = 0;
+    h = 0;
 }
 
-Rect::Rect(float x, float y, float w, float h)
-{
-	Rect::x = x;
-	Rect::y = y;
-	Rect::w = w;
-	Rect::h = h;
+Rect::Rect(float x, float y, float w, float h) {
+    Rect::x = x;
+    Rect::y = y;
+    Rect::w = w;
+    Rect::h = h;
 }
 
-Rect::~Rect()
-{
-
+Rect::~Rect() {
 }
 
-Vec2 Rect::GetXY()
-{
-	return Vec2(x,y);
+Vec2 Rect::GetXY() {
+    return Vec2(x, y);
 }
 
-void Rect::SetXY(Vec2 v)
-{
-	x = v.x;
-	y = v.y;
+void Rect::SetXY(Vec2 v) {
+    x = v.x;
+    y = v.y;
 }
 
-Vec2 Rect::GetWH()
-{
-	return Vec2(w,h);
+Vec2 Rect::GetWH() {
+    return Vec2(w, h);
 }
 
-void Rect::SetWH(Vec2 v)
-{
-	w = v.x;
-	h = v.y;
+void Rect::SetWH(Vec2 v) {
+    w = v.x;
+    h = v.y;
 }
 
-Vec2 Rect::GetCenter()
-{
-	return Vec2(x+w/2, y+h/2);
+Vec2 Rect::GetCenter() {
+    return Vec2(x + w/2, y + h/2);
 }
 
-Vec2 Rect::GetBottomLeft()
-{
-	return Vec2(x,y+h);
+Vec2 Rect::GetBottomLeft() {
+    return Vec2(x, y + h);
 }
 
-bool Rect::IsInside(Vec2 dot)
-{
-	return((dot.x > x) && (dot.x < x+w) && (dot.y > y) && (dot.y < y+h));
+bool Rect::IsInside(Vec2 dot) {
+    return((dot.x > x) && (dot.x < x + w) && (dot.y > y) && (dot.y < y + h));
 }
 
-Rect Rect::GetOverlap(const Rect &r)
-{
-	float minX = std::max(x, r.x);
-	float maxX = std::min(x+w, r.x+r.w);
-	float minY = std::max(y, r.y);
-	float maxY = std::min(y+h, r.y+r.h);
-	return Rect(minX, minY, maxX-minX, maxY-minY);
+Rect Rect::GetOverlap(const Rect &r) {
+    float minX = std::max(x, r.x);
+    float maxX = std::min(x + w, r.x + r.w);
+    float minY = std::max(y, r.y);
+    float maxY = std::min(y + h, r.y + r.h);
+    return Rect(minX, minY, maxX - minX, maxY - minY);
 }
 
-bool Rect::OverlapsWith(const Rect &r)
-{
-	float distX = x - r.x;
-	float distY = y - r.y;
-	if(distX >= r.w || -distX >= w || distY >= r.h || -distY >= h)
-		return false;
-	return true;
+bool Rect::OverlapsWith(const Rect &r) {
+    float distX = x - r.x;
+    float distY = y - r.y;
+    if (distX >= r.w || -distX >= w || distY >= r.h || -distY >= h) {
+        return false;
+    }
+    return true;
 }
 
-Rect Rect::operator+(const Vec2& v1)
-{
-	return Rect(x+v1.x, y+v1.y, w+v1.x, h+v1.y);
+Rect Rect::operator+(const Vec2& v1) {
+    return Rect(x + v1.x, y + v1.y, w + v1.x, h + v1.y);
 }
 
-Rect Rect::operator-(const Vec2& v1)
-{
-	return Rect(x-v1.x, y-v1.y, w-v1.x, h-v1.y);
+Rect Rect::operator-(const Vec2& v1) {
+    return Rect(x - v1.x, y - v1.y, w - v1.x, h - v1.y);
 }
--- src/Turret.cpp
+++ src/Turret.cpp
@@ -1,154 +1,120 @@
+// Copyright 2017 Neon Edge Game.
+
 #include "Turret.h"
 #include "AIMovingOnGroudGraphicsComponent.h"
 #include "StageState.h"
 #include "Gallahad.h"
 #include "Projectile.h"
 
-Turret::Turret(int x, int y):
-	Character(x,y),
-	radius(),
-	looking(1500),
-	idle(1500)
-{
-	name = "Turret";
-	graphicsComponent = new AIMovingOnGroudGraphicsComponent("Turret");
-	box.SetWH(graphicsComponent->GetSize());
-	attackCD.SetLimit(300);
-	idle.Start();
-	hitpoints = 2;
+Turret::Turret(int x, int y): Character(x, y), radius(), looking(1500), idle(1500) {
+    name = "Turret";
+    graphicsComponent = new AIMovingOnGroudGraphicsComponent("Turret");
+    box.SetWH(graphicsComponent->GetSize());
+    attackCD.SetLimit(300);
+    idle.Start();
+    hitpoints = 2;
 }
 
-Turret::~Turret()
-{
-
+Turret::~Turret() {
 }
 
-void Turret::Attack()
-{
-	//Starts attack timer
-	attackCD.Start();
-	//Generates attack object
-	StageState::AddObject(new Projectile(this, Vec2(0.4, 0), 400, 1, false));
+void Turret::Attack() {
+	// Starts attack timer.
+    attackCD.Start();
+	// Generates attack object.
+    StageState::AddObject(new Projectile(this, Vec2(0.4, 0), 400, 1, false));
 }
 
-void Turret::NotifyTileCollision(int tile, GameObject::Face face)
-{
-	/*if(tile >= SOLID_TILE && (face == LEFT || face == RIGHT))
-	{
-		if(physicsComponent.velocity.y >= 0.6)
-		{
-			physicsComponent.velocity.y = -0.5;
-		}
-	}*/
+void Turret::NotifyTileCollision(int tile, GameObject::Face face) {
+ /* if (tile >= SOLID_TILE && (face == LEFT || face == RIGHT)) {
+        if (physicsComponent.velocity.y >= 0.6) {
+            physicsComponent.velocity.y = -0.5;
+        }
+    } */
 }
 
-void Turret::UpdateTimers(float dt)
-{
-	Character::UpdateTimers(dt);
-	if(!stunned.IsRunning())
-	{
-		if(looking.IsRunning())
-		{
-			looking.Update(dt);
-			if(!looking.IsRunning())
-				idle.Start();
-		}
-		else if(idle.IsRunning())
-		{
-			idle.Update(dt);
-			if(!idle.IsRunning())
-				looking.Start();
-		}
-	}
+void Turret::UpdateTimers(float dt) {
+    Character::UpdateTimers(dt);
+    if (!stunned.IsRunning()) {
+        if (looking.IsRunning()) {
+            looking.Update(dt);
+            if (!looking.IsRunning()) {
+                idle.Start();
+						}
+        } else if (idle.IsRunning()) {
+            idle.Update(dt);
+            if (!idle.IsRunning()) {
+                looking.Start();
+						}
+        }
+    }
 }
 
-void Turret::UpdateAI(float dt)
-{
-	radius = Rect(box.x-200, box.y-200, box.w+400, box.h+400);
-	if(StageState::GetPlayer())
-	{
-		if(!stunned.IsRunning())
-		{
-			Rect player = StageState::GetPlayer()->box;
-			bool visible = true;
-			if(StageState::GetPlayer()->Is("Gallahad"))
-			{
-				Gallahad* g = (Gallahad*) StageState::GetPlayer();
-				if(g->Hiding())
-				{
-					visible = false;
-				}
-			}
-			if(player.OverlapsWith(radius) && visible)
-			{
-				if(player.x < box.x )
-				{
-					physicsComponent.velocity.x -= 0.003*dt;
-					if(box.x - physicsComponent.velocity.x*dt < player.x)
-					{
-						box.x = player.x;
-						physicsComponent.velocity.x = 0;
-					}
-					facing = LEFT;
-				}
-				else
-				{
-					physicsComponent.velocity.x += 0.003*dt;
-					if(box.x + physicsComponent.velocity.x*dt > player.x)
-					{
-						box.x = player.x;
-						physicsComponent.velocity.x = 0;
-					}
-					facing = RIGHT;
-				}
-				clamp(physicsComponent.velocity.x,-0.3f,0.3f);
+void Turret::UpdateAI(float dt) {
+    radius = Rect(box.x - 200, box.y - 200, box.w + 400, box.h + 400);
+    if (StageState::GetPlayer()) {
+        if (!stunned.IsRunning()) {
+            Rect player = StageState::GetPlayer()->box;
+						bool visible = true;
+            if (StageState::GetPlayer()->Is("Gallahad")) {
+                Gallahad* g = (Gallahad*) StageState::GetPlayer();
+                if (g->Hiding()) {
+                    visible = false;
+                }
+            }
+            if (player.OverlapsWith(radius) && visible) {
+                if (player.x < box.x ) {
+                    physicsComponent.velocity.x -= 0.003 * dt;
+                    if (box.x - physicsComponent.velocity.x * dt < player.x) {
+                        box.x = player.x;
+                        physicsComponent.velocity.x = 0;
+                    }
+                    facing = LEFT;
+                } else {
+                    physicsComponent.velocity.x += 0.003*dt;
+                    if (box.x + physicsComponent.velocity.x * dt > player.x) {
+                        box.x = player.x;
+                        physicsComponent.velocity.x = 0;
+                    }
+										facing = RIGHT;
+                }
+                clamp(physicsComponent.velocity.x,-0.3f,0.3f);
 
-				if(!Cooling())
-				{
-					Attack();
-				}
-			}
-			else if(looking.IsRunning() && looking.GetElapsed() == 0)
-			{
-				if(facing == LEFT)
-					physicsComponent.velocity.x = -0.2;
-				else
-					physicsComponent.velocity.x = 0.2;
-			}
-			else
-			{
-				if(idle.IsRunning() && idle.GetElapsed() == 0)
-				{
-					physicsComponent.velocity.x = 0;
-					if(facing == LEFT)
-					{
-						facing = RIGHT;
-					}
-					else
-					{
-						facing = LEFT;
-					}
-				}
-			}
-		}
-		else
-		{
-			physicsComponent.velocity.x = 0;
-		}
-	}
+                if (!Cooling()) {
+                    Attack();
+                }
+            } else if (looking.IsRunning() && looking.GetElapsed() == 0) {
+                if (facing == LEFT) {
+                physicsComponent.velocity.x = -0.2;
+							  } else {
+                    physicsComponent.velocity.x = 0.2;
+								}
+            } else {
+                if (idle.IsRunning() && idle.GetElapsed() == 0) {
+								    physicsComponent.velocity.x = 0;
+                    if (facing == LEFT) {
+						            facing = RIGHT;
+                    } else {
+                        facing = LEFT;
+                    }
+                }
+            }
+        } else {
+            physicsComponent.velocity.x = 0;
+        }
+    }
 }
 
-void Turret::Update(TileMap* world, float dt)
-{
-	UpdateTimers(dt);
-	UpdateAI(dt);
-	physicsComponent.Update(this,world,dt);
-	if(OutOfBounds(world))
-		SetPosition(Vec2(startingX,startingY));
-	graphicsComponent->Update(this,dt);
+void Turret::Update(TileMap* world, float dt) {
+    UpdateTimers(dt);
+    UpdateAI(dt);
+    physicsComponent.Update(this, world, dt);
+    if (OutOfBounds(world)) {
+		    SetPosition(Vec2(startingX, startingY));
+    }
+    graphicsComponent->Update(this, dt);
 }
 
-bool Turret::Is(std::string type)
-{
-	return (type=="Enemy");
+bool Turret::Is(std::string type) {
+    return (type == "Enemy");
 }
