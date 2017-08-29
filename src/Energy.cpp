1026f4bad0a0101c06f44f6401116109d8ba1956
lbruno55 <l_brunofidelis@yahoo.com.br>
2017-08-29 01:25:40 -0300

    Aplicação 3 da folha de estilo em Energy.cpp e InputManager.cpp

--- src/Energy.cpp
+++ src/Energy.cpp
@@ -1,54 +1,49 @@
+// Copyright 2017 Neon Edge Game.
+
 #include "Energy.h"
 #include "Camera.h"
 
-Energy::Energy(int x, int y, std::string sprite, float frameCount, float frameTime, bool loops, float lifetime, bool dies)
-{
-	name = "Energy";
-	sp = Sprite(sprite, frameCount, frameTime);
-	Vec2 size = sp.GetSize();
-	box = Rect(x, y, size.x, size.y);
-	endTimer = Timer(lifetime);
-	if(loops)
-		endTimer.Start();
-	Energy::loops = loops;
-	Energy::dies = dies;
+Energy::Energy(int x, int y, std::string sprite, float frameCount, float frameTime, bool loops,
+                float lifetime, bool dies) {
+    name = "Energy";
+    sp = Sprite(sprite, frameCount, frameTime);
+    Vec2 size = sp.GetSize();
+    box = Rect(x, y, size.x, size.y);
+    endTimer = Timer(lifetime);
+    if (loops) {
+        endTimer.Start();
+    }
+    Energy::loops = loops;
+    Energy::dies = dies;
 }
 
-Energy::~Energy()
-{
-
+Energy::~Energy() {
 }
 
-bool Energy::IsDead()
-{
-	return dead;
+bool Energy::IsDead() {
+    return dead;
 }
 
-void Energy::NotifyObjectCollision(GameObject* other)
-{
-	if(other->Is("Gallahad") || other->Is("Lancelot"))
-	{
-		if(dies)
-		{
-			dead = true;
-		}
-	}
+void Energy::NotifyObjectCollision(GameObject* other) {
+    if (other->Is("Gallahad") || other->Is("Lancelot")) {
+        if (dies) {
+            dead = true;
+        }
+    }
 }
 
-void Energy::UpdateTimers(float dt)
-{
-	endTimer.Update(dt);
+void Energy::UpdateTimers(float dt) {
+    endTimer.Update(dt);
 }
 
-void Energy::Update(TileMap* world, float dt)
-{
-	UpdateTimers(dt);
-	if(!loops && !endTimer.IsRunning())
-		dead = true;
-	sp.Update(dt);
+void Energy::Update(TileMap* world, float dt) {
+    UpdateTimers(dt);
+    if (!loops && !endTimer.IsRunning()) {
+        dead = true;
+    }
+    sp.Update(dt);
 }
 
-void Energy::Render()
-{
-	sp.Render(box.x - Camera::GetInstance().pos.x, box.y - Camera::GetInstance().pos.y);
+void Energy::Render() {
+    sp.Render(box.x - Camera::GetInstance().pos.x, box.y - Camera::GetInstance().pos.y);
 }
--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -1,122 +1,119 @@
+// Copyright 2017 Neon Edge Game.
+
 #include "InputManager.h"
 
 InputManager* InputManager::instance = nullptr;
 
-InputManager::InputManager():
-	mouseX(0),
-	mouseY(0),
-	updateCounter(0),
-	quitRequested(false),
-	mouseState{false},
-	mouseUpdate{0},
-	translationTable{SDLK_SPACE,SDLK_e,SDLK_q,SDLK_a,SDLK_d,SDLK_s,SDLK_w,SDLK_s,SDLK_z,SDLK_j,SDLK_k,SDLK_l}
-{
-
+InputManager::InputManager(): mouseX(0), mouseY(0), updateCounter(0), quitRequested(false),
+                              mouseState{false}, mouseUpdate{0},
+                              translationTable{SDLK_SPACE, SDLK_e, SDLK_q, SDLK_a, SDLK_d, SDLK_s,
+															SDLK_w, SDLK_s, SDLK_z, SDLK_j, SDLK_k, SDLK_l} {
 }
 
 InputManager::~InputManager() {
-
 }
 
 void InputManager::Update() {
-	SDL_Event event;
-	SDL_GetMouseState(&mouseX, &mouseY);
-	if(updateCounter < 100)
-		updateCounter++;
-	else
-		updateCounter = 0;
+    SDL_Event event;
+    SDL_GetMouseState(&mouseX, &mouseY);
+    if (updateCounter < 100) {
+        updateCounter + +;
+    } else {
+        updateCounter = 0;
+    }
 
-	while(SDL_PollEvent(&event)) {
-		if(event.key.repeat != 1) {
-			if(event.type == SDL_QUIT)
-				quitRequested = true;
-			if(event.type == SDL_MOUSEBUTTONDOWN) {
-				mouseState[event.button.button] = true;
-				mouseUpdate[event.button.button] = updateCounter;
-			}
-			if(event.type == SDL_MOUSEBUTTONUP) {
-				mouseState[event.button.button] = false;
-				mouseUpdate[event.button.button] = updateCounter;
-			}
-			if(event.type == SDL_KEYDOWN) {
-				keyState[event.key.keysym.sym] = true;
-				keyUpdate[event.key.keysym.sym] = updateCounter;
-				lastKey = event.key.keysym.sym;
-			}
-			if(event.type == SDL_KEYUP) {
-				keyState[event.key.keysym.sym] = false;
-				keyUpdate[event.key.keysym.sym] = updateCounter;
-			}
-		}
-	}
+    while (SDL_PollEvent(&event)) {
+        if (event.key.repeat != 1) {
+            if (event.type == SDL_QUIT) {
+                quitRequested = true;
+						}
+            if (event.type == SDL_MOUSEBUTTONDOWN) {
+                mouseState[event.button.button] = true;
+                mouseUpdate[event.button.button] = updateCounter;
+            }
+            if (event.type == SDL_MOUSEBUTTONUP) {
+                mouseState[event.button.button] = false;
+                mouseUpdate[event.button.button] = updateCounter;
+            }
+            if (event.type == SDL_KEYDOWN) {
+                keyState[event.key.keysym.sym] = true;
+                keyUpdate[event.key.keysym.sym] = updateCounter;
+                lastKey = event.key.keysym.sym;
+            }
+            if (event.type == SDL_KEYUP) {
+                keyState[event.key.keysym.sym] = false;
+                keyUpdate[event.key.keysym.sym] = updateCounter;
+            }
+        }
+    }
 }
 
-int InputManager::TranslateKey(int key)
-{
-	return translationTable[key];
+int InputManager::TranslateKey(int key) {
+    return translationTable[key];
 }
 
-bool InputManager::KeyPress(int key,bool translate) {
-	if(translate)
-		key = TranslateKey(key);
-	return (keyUpdate[key] == updateCounter) ? (keyState[key]) : false;
+bool InputManager::KeyPress(int key, bool translate) {
+    if (translate) {
+    key = TranslateKey(key);
+    }
+    return (keyUpdate[key] == updateCounter) ? (keyState[key]) : false;
 }
 
-bool InputManager::KeyRelease(int key,bool translate) {
-	if(translate)
-		key = TranslateKey(key);
+bool InputManager::KeyRelease(int key, bool translate) {
+    if (translate) {
+        key = TranslateKey(key);
+    }
 	return (keyUpdate[key] == updateCounter) ? (!keyState[key]) : false;
 }
 
-bool InputManager::IsKeyDown(int key,bool translate) {
-	if(translate)
-		key = TranslateKey(key);
-	return keyState[key];
+bool InputManager::IsKeyDown(int key, bool translate) {
+    if (translate) {
+        key = TranslateKey(key);
+    }
+    return keyState[key];
 }
 
 bool InputManager::MousePress(int button) {
-	return (mouseUpdate[button] == updateCounter) ? (mouseState[button]) : false;
+    return (mouseUpdate[button] == updateCounter) ? (mouseState[button]) : false;
 }
 
 bool InputManager::MouseRelease(int button) {
-	return (mouseUpdate[button] == updateCounter) ? (!mouseState[button]) : false;
+    return (mouseUpdate[button] == updateCounter) ? (!mouseState[button]) : false;
 }
 
 bool InputManager::IsMouseDown(int button) {
-	return mouseState[button];
+    return mouseState[button];
 }
 
-void InputManager::SetTranslationKey(int src, int dest)
-{
-	translationTable[src]=dest;
+void InputManager::SetTranslationKey(int src, int dest) {
+    translationTable[src]=dest;
 }
 
 int InputManager::GetMouseX() {
-	return mouseX;
+    return mouseX;
 }
 
 int InputManager::GetMouseY() {
-	return mouseY;
+    return mouseY;
 }
 
-int InputManager::GetTranslationKey(int key)
-{
-	return translationTable[key];
+int InputManager::GetTranslationKey(int key) {
+    return translationTable[key];
 }
 
-int InputManager::GetLastKey()
-{
-	int temp = lastKey;
-	lastKey = -1;
-	return temp;
+int InputManager::GetLastKey() {
+    int temp = lastKey;
+    lastKey = -1;
+    return temp;
 }
 
 bool InputManager::QuitRequested() {
-	return quitRequested;
+    return quitRequested;
 }
 
 InputManager& InputManager::GetInstance() {
-	if(instance == nullptr)
-		instance = new InputManager();
-	return *instance;
+    if(instance == nullptr) {
+        instance = new InputManager();
+    }
+    return *instance;
 }
