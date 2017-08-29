9cbcd9a24548733a10ce8c53d9853648ca7bfec7
Hugo Catarino <hugocatarino1@gmail.com>
2017-08-28 23:45:57 -0300

    Aplicando folha de estilo 3 State.cpp e TileMap.cpp

--- src/State.cpp
+++ src/State.cpp
@@ -2,64 +2,54 @@
 #include "Game.h"
 
 State::State():
-	bg(),
-	music(),
-	quitRequested(false)
-{
+    bg(),
+    music(),
+    quitRequested(false) {
 
 }
 
-State::~State()
-{
+State::~State() {
 
 }
 
-void State::Render()
-{
+void State::Render() {
 
 }
 
-void State::Update()
-{
+void State::Update() {
 
 }
 
-Vec2 State::CenterVertical(Sprite *sp)
-{
-	return CenterVertical(sp->GetWidth());
+Vec2 State::CenterVertical(Sprite *sp) {
+    return CenterVertical(sp->GetWidth());
 }
 
-Vec2 State::CenterVertical(int size)
-{
-	SDL_Point windowSize = Game::GetInstance().GetScreenSize();
-	return Vec2((windowSize.x-size)/2,0);
+Vec2 State::CenterVertical(int size) {
+    SDL_Point windowSize = Game::GetInstance().GetScreenSize();
+    return Vec2((windowSize.x-size)/2,0);
 }
 
-Vec2 State::CenterVertical(Vec2 size)
-{
-	return CenterVertical(size.x);
+Vec2 State::CenterVertical(Vec2 size) {
+    return CenterVertical(size.x);
 }
 
-Vec2 State::CenterHorizontal(Sprite *sp)
-{
-	return CenterHorizontal(sp->GetHeight());
+Vec2 State::CenterHorizontal(Sprite *sp) {
+    return CenterHorizontal(sp->GetHeight());
 }
 
-Vec2 State::CenterHorizontal(int size)
-{
-	SDL_Point windowSize = Game::GetInstance().GetScreenSize();
-	return Vec2(0,(windowSize.y-size)/2);
+Vec2 State::CenterHorizontal(int size) {
+    SDL_Point windowSize = Game::GetInstance().GetScreenSize();
+    return Vec2(0,(windowSize.y-size)/2);
 }
 
-Vec2 State::CenterHorizontal(Vec2 size)
-{
-	return CenterHorizontal(size.y);
+Vec2 State::CenterHorizontal(Vec2 size) {
+    return CenterHorizontal(size.y);
 }
 
 State* State::get() {
-	return this;
+    return this;
 }
 
 bool State::QuitRequested() {
-	return quitRequested;
+    return quitRequested;
 }
