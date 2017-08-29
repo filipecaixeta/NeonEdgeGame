f91cc2de95a4a6ba3cdd7c7123b78d1744a221c3
lbruno55 <l_brunofidelis@yahoo.com.br>
2017-08-29 02:15:57 -0300

    Aplicação 4 da folha de estilo GameObject.cpp LancelotInputComponent.cpp

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,104 +1,81 @@
+// Copyright 2017 Neon Edge Game.
+
 #include "GameObject.h"
 #include "Character.h"
 #include "Game.h"
 
-GameObject::~GameObject()
-{
-
+GameObject::~GameObject() {
 }
 
-bool GameObject::Is(std::string type)
-{
-	return (type == name);
+bool GameObject::Is(std::string type) {
+    return (type == name);
 }
 
-bool GameObject::IsDead()
-{
-	return isDead;
+bool GameObject::IsDead() {
+    return isDead;
 }
 
-bool GameObject::IsCharacter()
-{
-	return false;
+bool GameObject::IsCharacter() {
+    return false;
 }
 
-bool GameObject::IsPlayer()
-{
-	return false;
+bool GameObject::IsPlayer() {
+    return false;
 }
 
-Vec2 GameObject::GetPosition()
-{
-	return Vec2(box.x, box.y);
+Vec2 GameObject::GetPosition() {
+    return Vec2(box.x, box.y);
 }
 
-void GameObject::SetPosition(Vec2 pos)
-{
-	box.x = pos.x;
-	box.y = pos.y;
+void GameObject::SetPosition(Vec2 pos) {
+    box.x = pos.x;
+    box.y = pos.y;
 }
 
-void GameObject::NotifyTileCollision(int tile, Face face)
-{
-
+void GameObject::NotifyTileCollision(int tile, Face face) {
 }
 
-void GameObject::NotifyObjectCollision(GameObject* other)
-{
-
+void GameObject::NotifyObjectCollision(GameObject* other) {
 }
 
-void GameObject::SolidCollision(GameObject* other)
-{
-	if(IsCharacter())
-	{
-		Character* c = (Character*) this;
-		int y = box.y-c->physicsComponent.velocity.y*Game::GetInstance().GetDeltaTime();
-		if(footing != GROUNDED && y+box.h < other->box.y)
-		{
-			box.y = other->box.y-box.h-1;
-			c->physicsComponent.velocity.y = 0.6;
-			if(lastFooting != GROUNDED)
-				lastFooting = footing;
-			footing = GROUNDED;
-		}
-		else
-		{
-			if(c->physicsComponent.velocity.x < 0)
-			{
-				if(box.x < other->box.x+other->box.w && box.x+box.w > other->box.x+other->box.w)
-				{
-					box.x = other->box.x+other->box.w+1;
-				}
-			}
-			else if(c->physicsComponent.velocity.x > 0)
-			{
-				if(box.x+box.w > other->box.x && box.x < other->box.x)
-				{
-					box.x = other->box.x-box.w-1;
-				}
-			}
-		}
-		if(y > other->box.y+other->box.h)
-		{
-			box.y = other->box.y+other->box.h+1;
-			c->physicsComponent.velocity.y = 0;
-		}
-		c->graphicsComponent->Update(this,0);
-	}
+void GameObject::SolidCollision(GameObject* other) {
+    if (IsCharacter()) {
+        Character* c = (Character*) this;
+        int y = box.y - c->physicsComponent.velocity.y * Game::GetInstance().GetDeltaTime();
+        if (footing != GROUNDED && y + box.h < other->box.y) {
+            box.y = other->box.y - box.h - 1;
+            c->physicsComponent.velocity.y = 0.6;
+            if (lastFooting != GROUNDED) {
+                lastFooting = footing;
+						}
+            footing = GROUNDED;
+        } else {
+            if (c->physicsComponent.velocity.x < 0) {
+                if (box.x < other->box.x + other->box.w && box.x + box.w > other->box.x + other->box.w) {
+                    box.x = other->box.x + other->box.w + 1;
+                }
+            } else if (c->physicsComponent.velocity.x > 0) {
+                if (box.x + box.w > other->box.x && box.x < other->box.x) {
+                    box.x = other->box.x - box.w-1;
+                }
+            }
+        }
+
+        if (y > other->box.y + other->box.h) {
+            box.y = other->box.y + other->box.h + 1;
+            c->physicsComponent.velocity.y = 0;
+        }
+        c->graphicsComponent->Update(this, 0);
+    }
 }
 
-bool GameObject::GetColisionData(SDL_Surface** surface_, SDL_Rect& clipRect_, Vec2& pos_, bool& mirror)
-{
-	return false;
+bool GameObject::GetColisionData(SDL_Surface** surface_, SDL_Rect& clipRect_, Vec2& pos_, bool& mirror) {
+    return false;
 }
 
-void GameObject::DieAnimation()
-{
-	dieTimer.Start();
+void GameObject::DieAnimation() {
+    dieTimer.Start();
 }
 
-void GameObject::Render()
-{
-
+void GameObject::Render() {
 }
--- src/LancelotInputComponent.cpp
+++ src/LancelotInputComponent.cpp
@@ -1,98 +1,97 @@
+// Copyright 2017 Neon Edge Game.
+
 #include "LancelotInputComponent.h"
 #include "InputManager.h"
 #include "Lancelot.h"
 
-#define clamp(N,L,U) N=std::max(L,std::min(N,U))
+#define clamp(N, L, U) N = std::max(L, std::min(N, U))
 
-LancelotInputComponent::LancelotInputComponent()
-{
-
+LancelotInputComponent::LancelotInputComponent() {
 }
 
-void LancelotInputComponent::Update(Player* obj_, float dt_)
-{
-	InputComponent::Update(obj_,dt_);
-	InputManager &input = InputManager::GetInstance();
+void LancelotInputComponent::Update(Player* obj_, float dt_) {
+    InputComponent::Update(obj_, dt_);
+    InputManager &input = InputManager::GetInstance();
 
-	if(input.IsKeyDown(MOVE_LEFT_KEY, true))
-		MoveLeft();
-	else if(input.IsKeyDown(MOVE_RIGHT_KEY, true))
-		MoveRight();
-	else
-		StayStill();
-	if(obj->Crouching())
-		clamp(obj->physicsComponent.velocity.x,-0.2f,0.2f);
-	else
-		clamp(obj->physicsComponent.velocity.x,-0.4f,0.4f);
+    if (input.IsKeyDown(MOVE_LEFT_KEY, true)) {
+        MoveLeft();
+    } else if (input.IsKeyDown(MOVE_RIGHT_KEY, true)) {
+        MoveRight();
+    } else {
+        StayStill();
+    }
 
-	if(input.IsKeyDown(CROUCH_KEY,  true))
-		Crouch(true);
-	else
-		Crouch(false);
+    if (obj->Crouching()) {
+        clamp(obj->physicsComponent.velocity.x, -0.2f, 0.2f);
+    } else {
+        clamp(obj->physicsComponent.velocity.x, -0.4f, 0.4f);
+    }
 
-	if(input.KeyPress(ATTACK_KEY,  true))
-	{
-		if(input.IsKeyDown(MOVE_LEFT_KEY,  true) || input.IsKeyDown(MOVE_RIGHT_KEY,  true))
-		{	
-			if(obj_->skills[6])
-				Combo("Axe");
-			else
-				Combo("Chop");
-		}
-		else if(input.IsKeyDown(SDLK_w))
-		{
-			if(obj_->skills[5])
-				Combo("Sword");
-			else
-				Combo("Uppercut");
-		}
-		else
-		{
-			if(obj_->skills[4])
-				Combo("Spear");
-			else
-				Combo("Straight");
-		}
-		Attack();
-	}
-	
-	if(input.IsKeyDown(SPECIAL_KEY, true))
-		Block();
-	else
-		Stop();
+    if (input.IsKeyDown(CROUCH_KEY, true)) {
+        Crouch(true);
+    } else {
+        Crouch(false);
+    }
 
-	if(input.KeyPress(JUMP_KEY, true))
-		Jump();
+    if (input.KeyPress(ATTACK_KEY, true)) {
+        if (input.IsKeyDown(MOVE_LEFT_KEY, true) || input.IsKeyDown(MOVE_RIGHT_KEY, true)) {
+            if (obj_->skills[6]) {
+                Combo("Axe");
+            } else {
+                Combo("Chop");
+            }
+        } else if (input.IsKeyDown(SDLK_w)) {
+            if (obj_->skills[5]) {
+                Combo("Sword");
+            } else {
+                Combo("Uppercut");
+            }
+        } else {
+            if (obj_->skills[4]) {
+                Combo("Spear");
+            } else {
+                Combo("Straight");
+            }
+        }
+        Attack();
+    }
 
-	if(input.MousePress(LEFT_MOUSE_BUTTON))
-		Save(true);
-	if(input.MousePress(RIGHT_MOUSE_BUTTON))
-		Save(false);
+    if (input.IsKeyDown(SPECIAL_KEY, true)) {
+        Block();
+    } else {
+        Stop();
+    }
 
-	ProcessItems();
+    if (input.KeyPress(JUMP_KEY, true)) {
+        Jump();
+    }
+
+    if (input.MousePress(LEFT_MOUSE_BUTTON)) {
+        Save(true);
+    }
+
+    if (input.MousePress(RIGHT_MOUSE_BUTTON)) {
+        Save(false);
+    }
+
+    ProcessItems();
 }
 
-void LancelotInputComponent::Block()
-{
-	Lancelot *l = (Lancelot*) obj;
-	if(l->GetEnergy() > 0)
-	{
-		l->Block();
-	}
-	else
-	{
-		l->Stop();
-	}
+void LancelotInputComponent::Block() {
+    Lancelot *l = (Lancelot*) obj;
+    if (l->GetEnergy() > 0) {
+        l->Block();
+    } else {
+        l->Stop();
+    }
 }
 
-void LancelotInputComponent::Stop()
-{
-	Lancelot *l = (Lancelot*) obj;
-	l->Stop();
+void LancelotInputComponent::Stop() {
+    Lancelot *l = (Lancelot*) obj;
+    l->Stop();
 }
 
-void LancelotInputComponent::Combo(std::string c)
-{
-	Lancelot *l = (Lancelot*) obj;
-	l->Combo(c);
+void LancelotInputComponent::Combo(std::string c) {
+    Lancelot *l = (Lancelot*) obj;
+    l->Combo(c);
 }
