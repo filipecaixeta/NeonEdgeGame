e11ca0a2b4c10431891b6aedd97df34cda201f99
lbruno55 <l_brunofidelis@yahoo.com.br>
2017-09-18 10:05:53 -0300

    Aplicação 1 das técnicas de comentários: TurretPiece.cpp, TurretBoss.cpp

--- src/TurretBoss.cpp
+++ src/TurretBoss.cpp
@@ -1,14 +1,28 @@
-#include "TurretBoss.h"
-#include "StageState.h"
-#include "Cutscene.h"
+/**
+  Copyright 2017 Neon Edge Game
+  File Name: TurretBoss.cpp
+  Header File Name: TurretBoss.h
+  Class Name: TurretBoss
+  Objective: Defines the characteristics and behaviors of the TurretBoss.
+*/
 
 #include <cmath>
 #include <math.h>
 #include <cstdlib>
 #include <sys/time.h>
 
+#include "TurretBoss.h"
+#include "StageState.h"
+#include "Cutscene.h"
+
+/**
+    Objective: Constructor of the classe Turret Boss.
+    @param int x - Size in x of the TurretBoss.
+    @param int y - Size in x of the TurretBoss.
+    @return instance of TurretBoss.
+*/
 TurretBoss::TurretBoss(int x, int y):
-    Character(x,y),
+    Character(x, y),
     idle(1000),
     aim(3000),
     shoot(300) {
@@ -16,20 +30,25 @@ TurretBoss::TurretBoss(int x, int y):
     box.x = x;
     box.y = y;
     box.w = 0;
-    box.h = 0;
+    box.h = 0; // Sets the attributes x, y, width and height of the box of the TurretBoss.
     pieces = std::vector<TurretPiece*>();
     turrets = std::vector<TurretPiece*>();
-    facing = LEFT;
+    facing = LEFT; // Sets left to 'facing', initialing the TurretBoss facing to left.
     Damage(5);
     triggered = false;
     state = tIDLE;
     physicsComponent.SetKinetic(true);
-    physicsComponent.velocity.y = 0;
+    physicsComponent.velocity.y = 0; // Velocity in y is zero why the TurretBoss is fixed.
     timeval t1;
     gettimeofday(&t1, NULL);
     srand(t1.tv_usec * t1.tv_sec);
 }
 
+/**
+    Objective: Destructor of the class TurretBoss, destructing and releasing memory turret per turret.
+    @param none.
+    @return none.
+*/
 TurretBoss::~TurretBoss() {
     for (unsigned i = 0; i < pieces.size(); i++) {
         pieces[i]->Kill();
@@ -41,19 +60,40 @@ TurretBoss::~TurretBoss() {
     Game::GetInstance().AddState(new Cutscene(5, false));
 }
 
+/**
+    Objective: Adds turret piece on the TurretBoss.
+    @param TurretPiece* piece - piece of the type TurretPiece for add in the final of TurretBoss.
+    @return void.
+*/
 void TurretBoss::AddPiece(TurretPiece* piece) {
     pieces.emplace_back(piece);
 }
 
+/**
+    Objective: Adds a turret piece on the TurretBoss.
+    @param TurretPiece* turret - piece of the type TurretPiece for add in the final of TurretBoss.
+    @return void.
+*/
 void TurretBoss::AddTurret(TurretPiece* turret) {
-    turrets.emplace_back(turret);
+    turrets.emplace_back(turret); // Adds a turret piece on the final of vector turret.
 }
 
+/**
+    Objective: none.
+    @param GameObject* other.
+    @return void.
+*/
 void TurretBoss::NotifyObjectCollision(GameObject* other) {
 }
 
+/**
+    Objective: Updates the AI of the TurretBoss, from a radius.
+    @param float dt.
+    @return void.
+*/
 void TurretBoss::UpdateAI(float dt) {
-    radius = Rect(box.x-500, box.y-500, box.w+1000, box.h+1000);
+    // Defines the radius of vision of the TurretBoss as a rectangle.
+    radius = Rect(box.x - 500, box.y - 500, box.w + 1000, box.h + 1000);
     if (StageState::GetPlayer()) {
         Rect player = StageState::GetPlayer()->box;
         if (player.OverlapsWith(radius)) {
@@ -61,14 +101,16 @@ void TurretBoss::UpdateAI(float dt) {
         }
 
         if (triggered) {
+            // If the state of the TurretBoss is 'idle' the aim is selected and the new state is 'aiming'.
             if (state == tIDLE) {
                 if (!idle.IsRunning()) {
                     aim.Start();
                     state = tAIMING;
-                    turret1 = rand()%6;
-                    turret2 = rand()%6;
+                    // Sets differents random numbers to turret1 and turret2.
+                    turret1 = rand() % 6;
+                    turret2 = rand() % 6;
                     while (turret2 == turret1) {
-                        turret2 = rand()%6;
+                        turret2 = rand() % 6;
                     }
                 }
             }
@@ -76,39 +118,42 @@ void TurretBoss::UpdateAI(float dt) {
                 for (unsigned i = 0; i < turrets.size(); i++) {
                     if (i == turret1 || i == turret2) {
                         Rect turret = turrets[i]->box;
+                        // From absolute values of x and y of the Turret and Player, sets the angle for attack.
                         float dx = std::abs(turret.GetCenter().x - player.GetCenter().x);
                         float dy = turret.GetCenter().y - player.GetCenter().y;
-                        float angle = atan(dy/dx) * 180 / M_PI;
+                        float angle = atan(dy / dx) * 180 / M_PI;
+                        // If the turret angle is less than zero, other turret with valid angle is selected to attack.
                         if (angle < 0) {
-                            if (turrets[i]->Rotation()-(0.03*dt) < angle) {
+                            if (turrets[i]->Rotation() - (0.03 * dt) < angle) {
                                 turrets[i]->Rotate(angle);
                             }
                             else {
-                                turrets[i]->Rotate(turrets[i]->Rotation()-(0.03*dt));
+                                turrets[i]->Rotate(turrets[i]->Rotation() - (0.03 * dt));
                             }
                         } else {
-                            if (turrets[i]->Rotation()+(0.03*dt) > angle) {
+                            if (turrets[i]->Rotation() + (0.03 * dt) > angle) {
                                 turrets[i]->Rotate(angle);
                             } else {
-                                turrets[i]->Rotate(turrets[i]->Rotation()+(0.03*dt));
+                                turrets[i]->Rotate(turrets[i]->Rotation() + (0.03 * dt));
                             }
                         }
                     } else {
                         if (turrets[i]->Rotation() < 0) {
-                            if (turrets[i]->Rotation()+(0.01*dt) > 0) {
+                            if (turrets[i]->Rotation() + (0.01 * dt) > 0) {
                                 turrets[i]->Rotate(0);
                             } else {
-                                turrets[i]->Rotate(turrets[i]->Rotation()+(0.01*dt));
+                                turrets[i]->Rotate(turrets[i]->Rotation() + (0.01 * dt));
                             }
                         } else {
-                            if (turrets[i]->Rotation()-(0.01*dt) < 0) {
+                            if (turrets[i]->Rotation() - (0.01 * dt) < 0) {
                                 turrets[i]->Rotate(0);
                             } else {
-                                turrets[i]->Rotate(turrets[i]->Rotation()-(0.01*dt));
+                                turrets[i]->Rotate(turrets[i]->Rotation() - (0.01 * dt));
                             }
                         }
                     }
                 }
+                // If there is a target the turret starts the attack, altering your state to 'shooting'.
                 if (!aim.IsRunning()) {
                     shoot.Start();
                     state = tSHOOTING;
@@ -117,6 +162,7 @@ void TurretBoss::UpdateAI(float dt) {
             if (state == tSHOOTING) {
                 turrets[turret1]->Shoot();
                 turrets[turret2]->Shoot();
+                // If the turret isnt shooting the actual state is 'idle'.
                 if (!shoot.IsRunning()) {
                     idle.Start();
                     state = tIDLE;
@@ -126,21 +172,39 @@ void TurretBoss::UpdateAI(float dt) {
     }
 }
 
+/**
+    Objective: Function responsible to update the Timer of the behaviors of the TurretBoss.
+    @param float dt - The amount of time the Turret Boss stay in 'idle' state, 'aim' state or 'shoot' state.
+    @return void.
+*/
 void TurretBoss::UpdateTimers(float dt) {
     Character::UpdateTimers(dt);
+    // Updates the idle, aim and shoot timer.
     idle.Update(dt);
     aim.Update(dt);
     shoot.Update(dt);
 }
 
+/**
+    Objective: Function responsible to update TurretBoss GameObject, but overloaded with the world object.
+    @param TileMap* world.
+    @param float dt.
+    @return void.
+*/
 void TurretBoss::Update(TileMap* world, float dt) {
     UpdateTimers(dt);
     UpdateAI(dt);
     physicsComponent.Update(this, world, dt);
+    // If the tower is outside the boundaries of the map, its position is resetted.
     if (OutOfBounds(world)) {
         SetPosition(Vec2(startingX, startingY));
     }
 }
 
+/**
+    Objective: none.
+    @param none.
+    @return void.
+*/
 void TurretBoss::Render() {
 }
--- src/TurretPiece.cpp
+++ src/TurretPiece.cpp
@@ -1,13 +1,28 @@
-// Copyright 2017 Neon Edge Game
+/**
+  Copyright 2017 Neon Edge Game
+  File Name: TurretPiece.cpp
+  Header File Name: TurretPiece.h
+  Class Name: TurretPiece
+  Objective: Defines the behavior and actions of a piece of the TurretBoss.
+*/
 
 #include <cmath>
 #include <math.h>
+
 #include "TurretPiece.h"
 #include "StageState.h"
 #include "Camera.h"
 #include "Player.h"
 #include "Projectile.h"
 
+/**
+    Objective: Constructor of the class TurretPiece.
+    @param Character *center.
+    @param int x - Size in x of the TurretPiece.
+    @param int y - Size in y of the TurretPiece.
+    @param int type.
+    @return instance of TurretPiece
+*/
 TurretPiece::TurretPiece(Character* center, int x, int y, int type):
     Character(0, 0) {
         name = "TurretBoss";
@@ -27,6 +42,11 @@ TurretPiece::TurretPiece(Character* center, int x, int y, int type):
 TurretPiece::~TurretPiece() {
 }
 
+/**
+    Objective: Defines the behavior of shoot of the TurretPiece.
+    @param none.
+    @return void.
+*/
 void TurretPiece::Shoot() {
     if (type > 2) {
         float Vx = cos(rotation * M_PI / 180) * 0.6;
@@ -35,22 +55,42 @@ void TurretPiece::Shoot() {
     }
 }
 
+/** Objective: Changes the rotation of the TurretPiece to the angle necessary.
+    @param float angle - angle of the shoot.
+    @return void.
+*/
 void TurretPiece::Rotate(float angle) {
     rotation = angle;
 }
 
+/** Objective: Returns the value of the rotation of the TurretPiece.
+    @param none.
+    @return float rotation.
+*/
 float TurretPiece::Rotation() {
     return rotation;
 }
 
+/** Objective: Returns the type of the TurretPiece.
+    @param none.
+    @return int type - type of the TurretPiece.
+*/
 int TurretPiece::Type() {
     return type;
 }
 
+/** Objective: Changes the state of the TurretPiece (alive or not).
+    @param none.
+    @return void.
+*/
 void TurretPiece::Kill() {
     isDead = true;
 }
 
+/** Objective: Manages the reactions of the box in a collision with another object.
+    @param GameObject* other - the object that is in collision.
+    @return void.
+*/
 void TurretPiece::NotifyObjectCollision(GameObject* other) {
     if (type == 0) {
         if (other->IsPlayer()) {
@@ -68,12 +108,22 @@ void TurretPiece::NotifyObjectCollision(GameObject* other) {
     }
 }
 
+/**
+    Objective: Function responsible to update TurretPiece GameObject, but overloaded with the object 'world'.
+    @param TileMap* world.
+    @param float dt.
+    @return void.
+*/
 void TurretPiece::Update(TileMap* world, float dt) {
     Vec2 size = graphicsComponent->GetSize();
     box.SetXY(Vec2(center->box.GetCenter().x + refX - (size.x / 2), center->box.GetCenter().y + refY - (size.y / 2)));
     graphicsComponent->Update(this, dt);
 }
 
+/** Objective: Draws the TurretPiece on the map.
+    @param none.
+    @return void.
+*/
 void TurretPiece::Render() {
     graphicsComponent->Render(GetPosition() - Camera::GetInstance().pos, rotation);
 }
