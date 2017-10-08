8cc90614f5476e7ea0b02bf9423a999e8888c6fb
Vinícius Cantuária <cantuariavc@gmail.com>
2017-10-08 15:25:15 -0300

    Aplicação 3 da técnica de não acredite em ninguém em Arthur.cpp

--- include/Arthur.h
+++ include/Arthur.h
@@ -8,24 +8,24 @@
 
 enum ArthurState{IDLE,SLASHING,DASHINGLEFT,DASHINGRIGHT,PUNCHING};
 
-class Arthur : public Character{
+class Arthur : public Character {
 public:
-	Arthur(int x, int y);
-	~Arthur();
-	void UpdateTimers(float dt);
-	void NotifyObjectCollision(GameObject* other);
-	void UpdateAI(float dt);
-	void Update(TileMap* world, float dt);
-	void Attack();
-	ArthurState GetState();
+    Arthur(int x, int y);
+    ~Arthur();
+    void UpdateTimers(float deltaTime);
+    void NotifyObjectCollision(GameObject *gameObject);
+    void UpdateAI(float deltaTime);
+    void Update(TileMap *world, float deltaTime);
+    void Attack();
+    ArthurState GetState();
 private:
-	Timer idle;
-	Timer slash;
-	Timer dash;
-	Timer punch;
-	Rect radius;
-	ArthurState state;
-	bool triggered;
+    Timer idle;
+    Timer slash;
+    Timer dash;
+    Timer punch;
+    Rect radius;
+    ArthurState arthurState;
+    bool triggered;
 };
 
 #endif /*ARTHUR_H*/
--- src/Arthur.cpp
+++ src/Arthur.cpp
@@ -1,222 +1,269 @@
 /**
-  Copyright (c) 2017 Neon Edge Game.
-  File Name: Arthur.cpp
-  Header File Name: Arthur.h
-  Class Name: Arthur
-  Objective: Defines the behavior and actions of Arthur, the boss of the second stage.
-
-*/
+ * Copyright (c) 2017 Neon Edge Game.
+ * File Name: Arthur.cpp
+ * Header File Name: Arthur.h
+ * Class Name: Arthur
+ * Objective: it defines the behavior and actions of Arthur, the boss of the second stage.
+ */
 
 #include "Arthur.h"
 #include "Cutscene.h"
 #include "StageState.h"
 
 /**
-    Objective: Constructor of the class Arthur.
-    @param int x - Initial position of the Arthur in X
-    @param int y - Initial position of the Arthur in Y.
-    @return instance of Arthur.
-
-*/
+ * Objective: it constructs Arthur object.
+ *
+ * @param int x - Initial position of the Arthur in X
+ * @param int y - Initial position of the Arthur in Y.
+ * @return instance of Arthur.
+ */
 Arthur::Arthur(int x, int y): Character(x, y), idle(3000), dash(1000), slash(1000), punch(1000) {
-    graphicsComponent = new ArthurGraphicsComponent("Arthur"); // Instances the graphics, adding the sprites.
+    // Instances the graphics, adding the sprites.
+    graphicsComponent = new ArthurGraphicsComponent("Arthur");
+
     name = "Arthur";
     box.SetWH(graphicsComponent->GetSize());
-    state = IDLE; // Sets the initial state as 'idle'.
+    arthurState = IDLE; // Sets the initial state as 'idle'.
     triggered = false;
     Damage(9);
 }
 
-/**
-    Objective: Destructor of the class Arthur.
-    @param none.
-    @return none.
-
-*/
 Arthur::~Arthur() {
     Game::GetInstance().GetCurrentState()->quitRequested = true;
     Game::GetInstance().AddState(new Cutscene(7, false));
 }
 
 /**
-    Objective: Deal with the collisions with Arthur.
-    @param GameObject other - object of type GameObject that receives the actions when collide with Arthur.
-    @return none.
-
-*/
-void Arthur::NotifyObjectCollision(GameObject* other) {
-    Character::NotifyObjectCollision(other);
-}
-
-/**
-    Objective: Updates the AI of the Arthur, from a radius.
-    @param float dt - The amount of time the Arthur updates its graphics.
-    @return none.
-
-*/
-void Arthur::UpdateAI(float dt) {
-    // Defines the radius of vision of the Arthur as a rectangle.
-    radius = Rect(box.x - 800, box.y - 800, box.w + 1000, box.h + 1000);
-    if (StageState::GetPlayer()) {
-        Rect player = StageState::GetPlayer()->box;
-        if (player.OverlapsWith(radius)) {
-            triggered = true;
-        }
-        if (triggered) {
-            if (state == IDLE) {
-                physicsComponent.velocity.x = 0; // Sets the velocity in zero when the state is 'idle'.
-                if (player.x < box.x) {
-                    facing = LEFT; // When the state is idle and velocity is zero, turns sideways.
-                    // If 'idle' state is not running, this block changes the state of Arthur according to the position of the Player.
-                    if (!idle.IsRunning()) {
-                        if (box.x - player.x > 400) {
-                            dash.Start(); // Starts the 'dash' to left if the distance between the player and Arthur is greater than 400.
-                            state = DASHINGLEFT;
-                        } else if (box.x - player.x > 100) {
-                            punch.Start();  // Starts the 'punch' state if the distance between the player and Arthur is greater than 100.
-                            state = PUNCHING;
-                        } else {
-                            slash.Start();  // Starts the 'slash' state if the distance between the player and Arthur is less than 100.
-                            state = SLASHING;
-                        }
-                    }
-                }
-                if (player.x > box.x) {
-                    facing = RIGHT;
-                    // If 'idle' state is not running, this block changes the state of Arthur according to the position of the Player.
-                    if (!idle.IsRunning()) {
-                        if (player.x - box.x > 400) {
-                            dash.Start();
-                            state = DASHINGRIGHT;
-                        } else if (player.x - box.x > 100) {
-                            punch.Start();
-                            state = PUNCHING;
-                        } else {
-                            slash.Start();
-                            state = SLASHING;
-                        }
-                    }
-                }
-            } else if (state == DASHINGLEFT) {
-                facing = LEFT;
-                if (!dash.IsRunning()) {
-                    idle.Start(); // Starts the 'idle' state when is not dashing to left.
-                    state = IDLE;
-                }
-                if (dash.IsRunning()) {
-                    physicsComponent.velocity.x -= 0.006 * dt; // Defines the velocity when is dashing to left.
-                    Attack();
-                }
-                clamp(physicsComponent.velocity.x, -0.8f, 0.8f);
-            } else if (state == DASHINGRIGHT) {
-                facing = RIGHT;
-                if (!dash.IsRunning()) {
-                    idle.Start(); // Starts the 'idle' state when is not dashing to right.
-                    state = IDLE;
-                }
-                if (dash.IsRunning()) {
-                    physicsComponent.velocity.x += 0.006 * dt; // Defines the velocity when is dashing to right.
-                    Attack();
-                }
-                clamp(physicsComponent.velocity.x, -0.8f, 0.8f);
-            } else if (state == SLASHING) {
-                if (player.x < box.x) {
-                    facing = LEFT;
-                    if (!slash.IsRunning()) {
-                        idle.Start(); // Starts the 'idle' state when is not slashing to left.
-                        state = IDLE;
-                    }
-                    if (slash.IsRunning()) {
-                        Attack(); // The slash is a attack. Starts attack when slash is running.
-                    }
-                }
-                if (player.x > box.x) {
-                    facing = RIGHT;
-                    if (!slash.IsRunning()) {
-                        idle.Start(); // Starts the 'idle' state when is not slashing to right.
-                        state = IDLE;
-                    }
-                    if (slash.IsRunning()) {
-                        Attack(); // The slash is a attack. Starts attack when slash is running.
-                    }
-                }
-            } else if (state == PUNCHING) {
-                if (player.x < box.x) {
-                    facing = LEFT;
-                    if (!punch.IsRunning()) {
-                        idle.Start(); // Starts the 'idle' state when is not punching to left.
-                        state = IDLE;
-                    }
-                    if (punch.IsRunning()) {
-                    }
-                }
-                if (player.x > box.x) {
-                    facing = RIGHT;
-                    if (!punch.IsRunning()) {
-                        idle.Start(); // Starts the 'idle' state when is not punching to right.
-                        state = IDLE;
-                    }
-                    if (punch.IsRunning()) {
-                    }
-                }
-            }
-        }
+ * Objective: it deals with the collisions with Arthur.
+ *
+ * @param GameObject *gameObject - GameObject object that receives the actions when
+ *                                 collide with Arthur.
+ * @return none.
+ */
+void Arthur::NotifyObjectCollision(GameObject *gameObject) {
+    if (gameObject) {
+        Character::NotifyObjectCollision(gameObject);
+    } else {
+        // It does nothing.
     }
 }
 
 /**
-    Objective: Starts the Arthur attack.
-    @param none.
-    @return none.
+ * Objective: it updates the AI of the Arthur, from a radius.
+ *
+ * @param float deltaTime - The amount of time the Arthur updates its graphics.
+ * @return none.
+ */
+void Arthur::UpdateAI(float deltaTime) {
+    if (deltaTime >= FLOAT_MIN_SIZE && deltaTime <= FLOAT_MAX_SIZE) {
+        // Defines the radius of vision of the Arthur as a rectangle.
+        radius = Rect(box.x - 800, box.y - 800, box.w + 1000, box.h + 1000);
+        if (StageState::GetPlayer()) {
+            Rect player = StageState::GetPlayer()->box;
+            if (player.OverlapsWith(radius)) {
+                triggered = true;
+                if (arthurState == IDLE) {
+                    // It sets the velocity in zero when the state is 'idle'.
+                    physicsComponent.velocity.x = 0;
+                    if (player.x < box.x) {
+                        // When the state is idle and velocity is zero, turns sideways.
+                        facing = LEFT;
+                        // If 'idle' state is not running, this block changes the state of Arthur
+                        // according to the position of the Player.
+                        if (!idle.IsRunning()) {
+                            if (box.x - player.x > 400) {
+                                dash.Start();  // It starts the 'dash' to left if the distance
+                                               // between the player and Arthur is greater than 400.
+                                arthurState = DASHINGLEFT;
+                            } else if (box.x - player.x > 100) {
+                                punch.Start();  // It starts the 'punch' state if the distance
+                                                // between the player and Arthur is greater than 100.
+                                arthurState = PUNCHING;
+                            } else {
+                                slash.Start();  // It starts the 'slash' state if the distance
+                                                // between the player and Arthur is less than 100.
+                                arthurState = SLASHING;
+                            }
+                        } else {
+                            // It does nothing.
+                        }
+                    } else if (player.x > box.x) {
+                        facing = RIGHT;
+                        // If 'idle' state is not running,this block changes the state of Arthur
+                        // according to the position of the Player.
+                        if (!idle.IsRunning()) {
+                            if (player.x - box.x > 400) {
+                                dash.Start();
+                                arthurState = DASHINGRIGHT;
+                            } else if (player.x - box.x > 100) {
+                                punch.Start();
+                                arthurState = PUNCHING;
+                            } else {
+                                slash.Start();
+                                arthurState = SLASHING;
+                            }
+                        } else {
+                            // It does nothing.
+                        }
+                    } else {
+                        // It does nothing.
+                    }
+                } else if (arthurState == DASHINGLEFT) {
+                    facing = LEFT;
+                    if (!dash.IsRunning()) {
+                        idle.Start();  // It starts the 'idle' state when is not dashing to left.
+                        arthurState = IDLE;
+                    } else if (dash.IsRunning()) {
+                        // Defines the velocity when is dashing to left.
+                        physicsComponent.velocity.x -= 0.006 * deltaTime;
 
-*/
+                        Attack();
+                    } else {
+                        // It does nothing.
+                    }
+                    clamp(physicsComponent.velocity.x, -0.8f, 0.8f);
+                } else if (arthurState == DASHINGRIGHT) {
+                    facing = RIGHT;
+                    if (!dash.IsRunning()) {
+                        idle.Start();  // It starts the 'idle' state when is not dashing to right.
+                        arthurState = IDLE;
+                    } else if (dash.IsRunning()) {
+                        // It defines the velocity when is dashing to right.
+                        physicsComponent.velocity.x += 0.006 * deltaTime;
+
+                        Attack();
+                    } else {
+                        // It does nothing.
+                    }
+                    clamp(physicsComponent.velocity.x, -0.8f, 0.8f);
+                } else if (arthurState == SLASHING) {
+                    if (player.x < box.x) {
+                        facing = LEFT;
+                        if (!slash.IsRunning()) {
+                            // It starts the 'idle' state when is not slashing to left.
+                            idle.Start();
+
+                            arthurState = IDLE;
+                        } else if (slash.IsRunning()) {
+                            Attack(); // The slash is a attack. Starts attack when slash is running.
+                        } else {
+                            // It does nothing.
+                        }
+                    } else if (player.x > box.x) {
+                        facing = RIGHT;
+                        if (!slash.IsRunning()) {
+                            // It starts the 'idle' state when is not slashing to right.
+                            idle.Start();
+
+                            arthurState = IDLE;
+                        } else if (slash.IsRunning()) {
+                            Attack(); // It starts attack when slash is running.
+                        } else {
+                            // It does nothing.
+                        }
+                    } else {
+                        // It does nothing.
+                    }
+                } else if (arthurState == PUNCHING) {
+                    if (player.x < box.x) {
+                        facing = LEFT;
+                        if (!punch.IsRunning()) {
+                            idle.Start();  // Starts the 'idle' state when is not punching to left.
+                            arthurState = IDLE;
+                        } else {
+                            // It does nothing.
+                        }
+                    } else if (player.x > box.x) {
+                        facing = RIGHT;
+                        if (!punch.IsRunning()) {
+                            idle.Start();  // Starts the 'idle' state when is not punching to right.
+                            arthurState = IDLE;
+                        } else {
+                            // It does nothing.
+                        }
+                    } else {
+                        // It does nothing.
+                    }
+                } else {
+                    // It does nothing.
+                }
+            } else {
+                // It does nothing.
+            }
+        } else {
+            // It does nothing.
+        }
+    } else {
+        // It does nothing.
+    }
+}
+
+/**
+ * Objective: it starts the Arthur attack.
+ *
+ * @param none.
+ * @return none.
+ */
 void Arthur::Attack() {
     attacking.Start();
 }
 
 /**
-    Objective: Updates the timers of 'idle', 'slash', 'dash' and 'punch' states.
-    @param float dt - Amount of time the Arthur remains in some state.
-    @return none.
-
-*/
-void Arthur::UpdateTimers(float dt) {
-    attacking.SetLimit(60);
-    idle.Update(dt);
-    slash.Update(dt);
-    dash.Update(dt);
-    punch.Update(dt);
-    if (attacking.GetElapsed() == 1) {
-        attacking.Reset();
-        attackCD.Start();
+ * Objective: it updates the timers of 'idle', 'slash', 'dash' and 'punch' states.
+ *
+ * @param float deltaTime - Amount of time the Arthur remains in some state.
+ * @return none.
+ */
+void Arthur::UpdateTimers(float deltaTime) {
+    if (deltaTime >= FLOAT_MIN_SIZE && deltaTime <= FLOAT_MAX_SIZE) {
+        attacking.SetLimit(60);
+        idle.Update(deltaTime);
+        slash.Update(deltaTime);
+        dash.Update(deltaTime);
+        punch.Update(deltaTime);
+        if (attacking.GetElapsed() == 1) {
+            attacking.Reset();
+            attackCD.Start();
+        } else {
+            // It does nothing.
+        }
+        attackCD.Update(deltaTime);
+    } else {
+        // It does nothing.
     }
-    attackCD.Update(dt);
 }
 
 /**
-    Objective: Function responsible to update Arthur GameObject, but overloaded with the world object.
-    @param TileMap* world.
-    @param float dt - Time the Arthur updates its timers, in nanoseconds.
-    @return none.
-
-*/
-void Arthur::Update(TileMap* world, float dt) {
-    UpdateTimers(dt);
-    UpdateAI(dt);
-    physicsComponent.Update(this, world, dt);
-    // If the Arthur is outside the boundaries of the map, its position is resetted.
-    if (OutOfBounds(world)) {
-        SetPosition(Vec2(startingX, startingY));
+ * Objective: it updates Arthur GameObject, but overloaded with the world object.
+ *
+ * @param TileMap *world.
+ * @param float deltaTime - Time the Arthur updates its timers, in nanoseconds.
+ * @return none.
+ */
+void Arthur::Update(TileMap *world, float deltaTime) {
+    if (world && (deltaTime >= FLOAT_MIN_SIZE && deltaTime <= FLOAT_MAX_SIZE)) {
+        UpdateTimers(deltaTime);
+        UpdateAI(deltaTime);
+        physicsComponent.Update(this, world, deltaTime);
+        // If the Arthur is outside the boundaries of the map, its position is resetted.
+        if (OutOfBounds(world)) {
+            SetPosition(Vec2(startingX, startingY));
+        } else {
+            // It does nothing.
+        }
+        graphicsComponent->Update(this, deltaTime);
+    } else {
+        // It does nothing.
     }
-    graphicsComponent->Update(this, dt);
 }
 
 /**
-    Objective: Returns the actual state of Arthur.
-    @param none.
-    @return state - Enum that represents a status: 0-IDLE, 1-SLASHING, 2-DASHINGLEFT, 3-DASHINGRIGHT, 4-PUNCHING
-
-*/
+ * Objective: it returns the actual state of Arthur.
+ *
+ * @param none.
+ * @return arthurState - Enum that represents a status:
+ *                       0 - IDLE, 1 - SLASHING, 2 - DASHINGLEFT, 3 - DASHINGRIGHT, 4 - PUNCHING.
+ */
 ArthurState Arthur::GetState() {
-    return state;
+    return arthurState;
 }
