3636b23e2dd38ee5d13ff2e9f0c157e7dac8cee0
ulyssesBML <ulysses3353@gmail.com>
2017-10-09 20:51:26 -0300

    Aplicando tecnica não confie em ninguem 2

--- src/LancelotInputComponent.cpp
+++ src/LancelotInputComponent.cpp
@@ -29,81 +29,86 @@ LancelotInputComponent::LancelotInputComponent() {
  * @return none.
  */
 void LancelotInputComponent::Update(Player *player, float deltaTime) {
-    InputComponent::Update(player, deltaTime);
-    InputManager &input = InputManager::GetInstance();
-	float const VELOCITY_RANGED_LOW = 0.2f; 
-	float const VELOCITY_RANGED_HIGH = 0.4f; 
+	int const HOUR_IN_MILISECONDS = 3600000;
+	if(player != NULL & deltaTime < HOUR_IN_MILISECONDS){
+		InputComponent::Update(player, deltaTime);
+		InputManager &input = InputManager::GetInstance();
+		float const VELOCITY_RANGED_LOW = 0.2f; 
+		float const VELOCITY_RANGED_HIGH = 0.4f; 
 	
-    if (input.IsKeyDown(MOVE_LEFT_KEY, true)) {
-        MoveLeft();
-    } else if (input.IsKeyDown(MOVE_RIGHT_KEY, true)) {
-        MoveRight();
-    } else {
-        StayStill();
-    }
+		if (input.IsKeyDown(MOVE_LEFT_KEY, true)) {
+			MoveLeft();
+		} else if (input.IsKeyDown(MOVE_RIGHT_KEY, true)) {
+			MoveRight();
+		} else {
+			StayStill();
+		}
 
-    if (player->IsCrouching()) {
-        clamp(player->physicsComponent.velocity.x, -1*(VELOCITY_RANGED_LOW), VELOCITY_RANGED_LOW);
-    } else {
-        clamp(player->physicsComponent.velocity.x, -1*(VELOCITY_RANGED_HIGH), VELOCITY_RANGED_HIGH);
-    }
+		if (player->IsCrouching()) {
+			clamp(player->physicsComponent.velocity.x, -1*(VELOCITY_RANGED_LOW), VELOCITY_RANGED_LOW);
+		} else {
+			clamp(player->physicsComponent.velocity.x, -1*(VELOCITY_RANGED_HIGH), VELOCITY_RANGED_HIGH);
+		}
 
-    if (input.IsKeyDown(CROUCH_KEY, true)) {
-        Crouch(true);
-    } else {
-        Crouch(false);
-    }
+		if (input.IsKeyDown(CROUCH_KEY, true)) {
+			Crouch(true);
+		} else {
+			Crouch(false);
+		}
 
-    if (input.KeyPress(ATTACK_KEY, true)) {
-        if (input.IsKeyDown(MOVE_LEFT_KEY, true) || input.IsKeyDown(MOVE_RIGHT_KEY, true)) {
-            if (player->skills[6]) {
-                Combo("Axe");
-            } else {
-                Combo("Chop");
-            }
-        } else if (input.IsKeyDown(SDLK_w)) {
-            if (player->skills[5]) {
-                Combo("Sword");
-            } else {
-                Combo("Uppercut");
-            }
-        } else {
-            if (player->skills[4]) {
-                Combo("Spear");
-            } else {
-                Combo("Straight");
-            }
-        }
-        Attack();
-    } else {
-        // It does nothing.
-    }
+		if (input.KeyPress(ATTACK_KEY, true)) {
+			if (input.IsKeyDown(MOVE_LEFT_KEY, true) || input.IsKeyDown(MOVE_RIGHT_KEY, true)) {
+				if (player->skills[6]) {
+					Combo("Axe");
+				} else {
+					Combo("Chop");
+				}
+			} else if (input.IsKeyDown(SDLK_w)) {
+				if (player->skills[5]) {
+					Combo("Sword");
+				} else {
+					Combo("Uppercut");
+				}
+			} else {
+				if (player->skills[4]) {
+					Combo("Spear");
+				} else {
+					Combo("Straight");
+				}
+			}
+			Attack();
+		} else {
+			// It does nothing.
+		}
 
-    if (input.IsKeyDown(SPECIAL_KEY, true)) {
-        Block();
-    } else {
-        Stop();
-    }
+		if (input.IsKeyDown(SPECIAL_KEY, true)) {
+			Block();
+		} else {
+			Stop();
+		}
 
-    if (input.KeyPress(JUMP_KEY, true)) {
-        Jump();
-    } else {
-        // It does nothing.
-    }
+		if (input.KeyPress(JUMP_KEY, true)) {
+			Jump();
+		} else {
+			// It does nothing.
+		}
 
-    if (input.MousePress(LEFT_MOUSE_BUTTON)) {
-        Save(true);
-    } else {
-        // It does nothing.
-    }
+		if (input.MousePress(LEFT_MOUSE_BUTTON)) {
+			Save(true);
+		} else {
+			// It does nothing.
+		}
 
-    if (input.MousePress(RIGHT_MOUSE_BUTTON)) {
-        Save(false);
-    } else {
-        // It does nothing.
-    }
+		if (input.MousePress(RIGHT_MOUSE_BUTTON)) {
+			Save(false);
+		} else {
+			// It does nothing.
+		}
 
-    ProcessItems();
+		ProcessItems();
+	} else {
+		//Do nothing
+	}
 }
 
 /**
@@ -114,10 +119,10 @@ void LancelotInputComponent::Update(Player *player, float deltaTime) {
  */
 void LancelotInputComponent::Block() {
 	int const NO_ENERGY = 0;
-    Lancelot *lancelot = (Lancelot*) player;
-    if (lancelot->GetEnergy() > NO_ENERGY) {
-        lancelot->StartBlock();
-    } else {
+	Lancelot *lancelot = (Lancelot*) player;
+	if (lancelot->GetEnergy() > NO_ENERGY) {
+		lancelot->StartBlock();
+	} else {
         lancelot->StopBlock();
     }
 }
@@ -139,7 +144,11 @@ void LancelotInputComponent::Stop() {
  * @param string c - combo name.
  * @return none.
  */
-void LancelotInputComponent::Combo(std::string c) {
+void LancelotInputComponent::Combo(std::string combo) {
+	if(combo != ""){
     Lancelot *lancelot = (Lancelot*) player;
-    lancelot->SetCombo(c);
+    lancelot->SetCombo(combo);
+	} else {
+		//Do Nothing
+	}
 }
