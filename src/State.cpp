6187a6cf1d9a7580c6e8598f380cc05f26388d99
Thiago Ribeiro <thiagoflames@gmail.com>
2017-10-09 11:04:20 -0300

    Aplicação 1 da técnica de não acredite em ninguém em State.cpp e TileSet.cpp

--- src/State.cpp
+++ src/State.cpp
@@ -1,9 +1,9 @@
 /**
-    Copyright 2017 Neon Edge Game
-    File Name: State.cpp
-    Header File Name: State.h
-    Class Name: State
-    Objective: manages the states.
+ * Copyright 2017 Neon Edge Game
+ * File Name: State.cpp
+ * Header File Name: State.h
+ * Class Name: State
+ * Objective: manages the states.
  */
 
 #include "State.h"
@@ -29,39 +29,48 @@ void State::Update() {
 }
 
 /*
-    Objective: center vertical position from sprite.
-    @param: Sprite* sp.
-    @return: Vec2 CenterVertical.
-
+ * Objective: center vertical position from sprite.
+ *
+ * @param: Sprite* sp.
+ * @return: Vec2 CenterVertical.
  */
 Vec2 State::CenterVertical(Sprite *sp) {
     return CenterVertical(sp->GetWidth());
 }
 
 /*
-    Objective: center vertical position from sprite.
-    @param: int size.
-    @return: Vec2.
+ * Objective: center vertical position from sprite.
+ *
+ * @param: int size.
+ * @return: Vec2.
  */
 Vec2 State::CenterVertical(int size) {
-    SDL_Point windowSize = Game::GetInstance().GetScreenSize();
-    return Vec2((windowSize.x-size)/2,0);
+    if (size >= INT_MIN_SIZE && size <= INT_MAX_SIZE) {
+        SDL_Point windowSize = Game::GetInstance().GetScreenSize();
+        return Vec2((windowSize.x-size)/2,0);
+    }
 }
 
 /*
-    Objective: center vertical position from sprite.
-    @param: Vec2 size.
-    @return: Vec2.
+ * Objective: center vertical position from sprite.
+ *
+ * @param: Vec2 size.
+ * @return: Vec2.
  */
 
 Vec2 State::CenterVertical(Vec2 size) {
-    return CenterVertical(size.x);
+    float vec_size_x;
+    vec_size_x = size.x;
+    if (vec_size_x >= FLOAT_MIN_SIZE && vec_size_x <= FLOAT_MAX_SIZE){
+        return CenterVertical(vec_size_x);
+    }
 }
 
 /*
-  Function Objective: center horizontal position from sprite.
-  param: none.
-  return: none.
+ * Objective: center horizontal position from sprite.
+ *
+ * @param: none.
+ * @return: none.
  */
 
 Vec2 State::CenterHorizontal(Sprite *sp) {
@@ -69,41 +78,48 @@ Vec2 State::CenterHorizontal(Sprite *sp) {
 }
 
 /*
-    Objective: center horizontal position from sprite.
-    @param: int size.
-    @return: Vec2.
+ * Objective: center horizontal position from sprite.
+ *
+ * @param: int size.
+ * @return: Vec2.
  */
 
 Vec2 State::CenterHorizontal(int size) {
-    SDL_Point windowSize = Game::GetInstance().GetScreenSize();
-    return Vec2(0,(windowSize.y-size)/2);
+    if (size >= INT_MIN_SIZE && size <= INT_MAX_SIZE) {
+        SDL_Point windowSize = Game::GetInstance().GetScreenSize();
+        return Vec2(0,(windowSize.y-size)/2);
+    }
 }
 
 /*
-    Objective: center horizontal position from sprite.
-    @param: Vec2 size.
-    @return: Vec2.
-
+ * Objective: center horizontal position from sprite.
+ *
+ * @param: Vec2 size.
+ * @return: Vec2.
  */
 Vec2 State::CenterHorizontal(Vec2 size) {
-    return CenterHorizontal(size.y);
+    float vec_size_y;
+    vec_size_y = size.y;
+    if (vec_size_y >= FLOAT_MIN_SIZE && vec_size_y <= FLOAT_MAX_SIZE){
+        return CenterVertical(vec_size_y);
+    }
 }
 
 /*
-    Objective: get state and return it.
-    @param: none.
-    @return: State* this state.
-
+ * Objective: get state and return it.
+ *
+ * @param: none.
+ * @return: State* this state.
  */
 State* State::get() {
     return this;
 }
 
 /*
-    Objective: set quit request state.
-    @param: none.
-    @return: bool quitRequested.
-
+ * Objective: set quit request state.
+ *
+ * @param: none.
+ * @return: bool quitRequested.
  */
 bool State::QuitRequested() {
     return quitRequested;
--- src/TileSet.cpp
+++ src/TileSet.cpp
@@ -1,99 +1,112 @@
 /**
-    Copyright 2017 Neon Edge Game
-    File Name: TileSet.cpp
-    Header File Name: TileSet.h
-    Class Name: TileSet
-    Objective: manages Tiles of rooms.
-    
+ * Copyright 2017 Neon Edge Game
+ * File Name: TileSet.cpp
+ * Header File Name: TileSet.h
+ * Class Name: TileSet
+ * Objective: manages Tiles of rooms.
  */
 
 #include "TileSet.h"
 
 /**
-    Objective: Constructor of the TileSet.
-    @param int tileWidth.
-    @param int tileHeight.
-    @param string file - Sprite file name.
-    @param int adjustX - Adjust tile in X axis.
-    @param int adjustY - Adjust tile in Y axis.
-    @return: Return a instance of TileSet
+ * Objective: Constructor of the TileSet.
 
-*/
+ * @param int tileWidth.
+ * @param int tileHeight.
+ * @param string file - Sprite file name.
+ * @param int adjustX - Adjust tile in X axis.
+ * @param int adjustY - Adjust tile in Y axis.
+ * @return: a instance of TileSet.
+ */
 TileSet::TileSet(int tileWidth, int tileHeight, std::string file, int adjustX, int adjustY) {
-    tileSet = Sprite(file); // Sprite file.
-    TileSet::tileWidth = tileWidth; // Width of TileSet.
-    TileSet::tileHeight = tileHeight; // Height of TileSet.
-    columns = tileSet.GetWidth()/tileWidth; // Number of columns in TileSet.
-    rows = tileSet.GetHeight()/tileHeight; // Number of rows in TileSet.
-    TileSet::adjustX = adjustX; // Adjust X axis.
-    TileSet::adjustY = adjustY; // Ajdust Y axis.
+    if (tileWidth >= INT_MIN_SIZE && tileWidth <= INT_MAX_SIZE ) {
+        tileSet = Sprite(file); // Sprite file.
+        TileSet::tileWidth = tileWidth; // Width of TileSet.
+        columns = tileSet.GetWidth()/tileWidth; // Number of columns in TileSet.
+    }
+    if (tileHeight >= INT_MIN_SIZE && tileHeight <= INT_MAX_SIZE) {
+        TileSet::tileHeight = tileHeight; // Height of TileSet.
+        rows = tileSet.GetHeight()/tileHeight; // Number of rows in TileSet.
+    }
+    if(adjustX >= INT_MIN_SIZE && adjustX <= INT_MAX_SIZE){
+        TileSet::adjustX = adjustX; // Adjust X axis.
+    }
+    if(adjustY >= INT_MIN_SIZE && adjustY <= INT_MAX_SIZE){
+        TileSet::adjustY = adjustY; // Ajdust Y axis.
+    }
 }
 
 /**
-    Function Objective: Destroct the class TileSet, delocalizing some memory used in the class and shutdown dependent libraries.
-    @param None.
-    @return: None.
-    
-*/
+ * Function Objective: Destroct the class TileSet, delocalizing some memory used in the class and
+ *                      shutdown dependent libraries.
+ *
+ * @param none.
+ * @return: none.
+ */
 TileSet::~TileSet() {
 
 }
 
 /**
-   Objective: Render TileSet.
-   @param int index -.
-   @param int x -.
-   @param int y -.
-   @return: None
+ * Objective: Render TileSet.
 
+ * @param int index -.
+ * @param int x -.
+ * @param int y -.
+ * @return: none.
 */
 void TileSet::Render(int index, int x, int y) {
-    // Valid if index is between 0 and end of matrix
-    if(index > -1 && index < columns*rows) {
-        tileSet.SetClip(index%columns*(tileWidth+adjustX), index/columns*(tileHeight+adjustY), tileWidth+adjustX, tileHeight+adjustY);
-        tileSet.Render(x-adjustX, y-adjustY);
+    if (index >= INT_MIN_SIZE && index <= INT_MAX_SIZE) {
+        if ((x >= INT_MIN_SIZE && x <= INT_MIN_SIZE) && (y >= INT_MIN_SIZE && y <= INT_MAX_SIZE)) {
+            // Valid if index is between 0 and end of matrix
+            if(index > -1 && index < columns*rows) {
+                tileSet.SetClip(index%columns*(tileWidth+adjustX), index/columns*(tileHeight+adjustY),
+                 tileWidth+adjustX, tileHeight+adjustY);
+                tileSet.Render(x-adjustX, y-adjustY);
+            }
+        }
     }
 }
 
 /**
-   Objective: Return a Sprite of TileSet.
-   @param None.
-   @param None.
-   @return: Sprite of TileSet.
-   
-*/
+ * Objective: Return a Sprite of TileSet.
+
+ * @param none.
+ * @param none.
+ * @return: sprite of TileSet.
+ */
 Sprite TileSet::GetTileSet() {
     return tileSet;
 }
 
 /**
-   Objective: Return number of tileWidth from TileSet.
-   @param None.
-   @param None.
-   @return: int tileWidth.
-   
+ * Objective: Return number of tileWidth from TileSet.
+
+ * @param none.
+ * @param none.
+ * @return: int tileWidth.
 */
 int TileSet::GetTileWidth() {
     return tileWidth;
 }
 
 /**
-   Objective: Return number of tileHeight from TileSet.
-   @param None.
-   @param None.
-   @return: int tileHeight.
-   
+ * Objective: Return number of tileHeight from TileSet.
+
+ * @param none.
+ * @param none.
+ * @return: int tileHeight.
 */
 int TileSet::GetTileHeight() {
     return tileHeight;
 }
 
 /**
-   Objective: Return Number of columns from TileSet.
-   @param None.
-   @param None.
-   @return: int columns.
-   
+ * Objective: Return Number of columns from TileSet.
+
+ * @param none.
+ * @param none.
+ * @return: int columns.
 */
 int TileSet::GetColumns() {
     return columns;
@@ -104,7 +117,7 @@ int TileSet::GetColumns() {
    @param int tileWidth.
    @param int tileHeight.
    @return: int rows.
-   
+
 */
 int TileSet::GetRows() {
     return rows;
