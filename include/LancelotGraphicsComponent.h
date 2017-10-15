a2f61177cb3062bf7dcd03f32b6f0597fd744372
Vinícius Cantuária <cantuariavc@gmail.com>
2017-10-15 20:41:45 -0200

    Aplicação 2 da técnica de funções atómicas em LancelotGraphicsComponent.cpp

--- include/LancelotGraphicsComponent.h
+++ include/LancelotGraphicsComponent.h
@@ -1,5 +1,7 @@
-#ifndef LANCELOTGRAPHICSCOMPONENT_H
-#define LANCELOTGRAPHICSCOMPONENT_H
+// Copyright (c) 2017 Neon Edge Game.
+
+#ifndef INCLUDE_LANCELOTGRAPHICSCOMPONENT_H_
+#define INCLUDE_LANCELOTGRAPHICSCOMPONENT_H_
 
 #include <string>
 
@@ -8,13 +10,20 @@
 #include "Sprite.h"
 #include "Character.h"
 #include "GraphicsComponent.h"
+#define FRAME_TIME 80
+#define LANCELOT_VELOCITY_ZERO 0
 
-class LancelotGraphicsComponent: public GraphicsComponent
-{
-public:
-	LancelotGraphicsComponent(std::string baseName_);
-	~LancelotGraphicsComponent();
-	void Update(GameObject* obj, float dt);
+
+
+class LancelotGraphicsComponent: public GraphicsComponent {
+ public:
+    LancelotGraphicsComponent(std::string baseNameParam);
+    ~LancelotGraphicsComponent();
+    void Update(GameObject* gameObject, float deltaTime);
+    void UpdateCharacterSpriteToAttacking(GameObject *gameObject);
+    void UpdateCharacterSpriteToBlocking(GameObject *gameObject);
+    void UpdateCharacterSpriteToCrouching(GameObject *gameObject);
+    void UpdateCharacterSpriteToDying(GameObject *gameObject);
 };
 
-#endif // LANCELOTGRAPHICSCOMPONENT_H
+#endif  // INCLUDE_LANCELOTGRAPHICSCOMPONENT_H_
