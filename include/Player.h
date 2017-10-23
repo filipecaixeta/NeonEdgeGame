249746c49904bc095afc9f988bf7c57c5b5c9e5d
ulyssesBML <ulysses3353@gmail.com>
2017-10-23 13:58:33 -0200

    Aplicando tecnica de inicialização de variaveis 2

--- include/Player.h
+++ include/Player.h
@@ -27,8 +27,8 @@ public:
     virtual void UpdateTimers(float dt);
     virtual void Update(TileMap* map,float dt);
 
-    enum {SkillBlocking3 = 0, SkillBlocking2, SkillBlocking1, Skill, SkillWeapon_1, SkillWeapon_2,
-        SkillWeapon_3};  // The skills options of the player.
+    enum {SkillBlocking3 = 0, SkillBlocking2 = 0, SkillBlocking1 = 0, Skill = 0, SkillWeapon_1 = 0, SkillWeapon_2 = 0,
+        SkillWeapon_3 = 0};  // The skills options of the player.
 
     InputComponent* inputComponent;
     ItensManager* itemManager;
@@ -37,10 +37,11 @@ public:
     int skillPoints;
 
 protected:
-    int energy;
-    Timer regenCD;
-    bool isCrouching;
-    bool isStading;
+	int const INITIAL_ENERGY_VALUE = 0;
+    int energy = INITIAL_ENERGY_VALUE;
+    Timer regenCD = 0;
+    bool isCrouching = false;
+    bool isStading = false;
 };
 
 #endif /* PLAYER_H_ */
