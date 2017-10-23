8ee22c4e9acd13889abbf14a5f6940f76ba76532
Eduardo Júnio Veloso Rodrigues <eduardojvr10@gmail.com>
2017-10-22 22:45:28 -0200

    Correção de bug

--- src/GraphicsComponent.cpp
+++ src/GraphicsComponent.cpp
@@ -45,9 +45,10 @@ bool GraphicsComponent::GetCharacterLeftDirection() {
 
 void GraphicsComponent::UpdateSprite(GameObject *gameObject, std::string spriteParam) {
     if (sprite != sprites[spriteParam]) {
+        int w = sprite->GetWidth();                
+        int h = sprite->GetHeight();        
         sprite = sprites[spriteParam];
         surface = surfaces[spriteParam];
-        int w = sprite->GetWidth();        
         if (gameObject->footing == GameObject::LEFT_WALLED) {
             gameObject->box.x += 0;
         } else if (gameObject->footing == GameObject::RIGHT_WALLED) {
@@ -56,7 +57,6 @@ void GraphicsComponent::UpdateSprite(GameObject *gameObject, std::string spriteP
             gameObject->box.x += (w - sprite->GetWidth()) / 2;
         }
 
-        int h = sprite->GetHeight();        
         gameObject->box.y += h - sprite->GetHeight();
         gameObject->box.SetWH(GetSize());
         if (!sprite->Loops()) {
