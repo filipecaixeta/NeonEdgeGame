64f4449340b3993d32b447070305287e91735400
ulyssesBML <ulysses3353@gmail.com>
2017-10-23 13:58:57 -0200

    Aplicando tecnica de inicialização de variaveis 3

--- include/Sprite.h
+++ include/Sprite.h
@@ -4,6 +4,8 @@
 #define FLOAT_MAX_SIZE 3.4e+38f
 #define INT_MIN_SIZE -32768
 #define INT_MAX_SIZE 32767
+#define INT_MAX_SIZE 32767
+
 
 #include <string>
 
@@ -12,7 +14,7 @@
 
 class Sprite {
 private:
-	SDL_Texture* texture;
+	SDL_Texture* texture = NULL;
 	SDL_Rect clipRect;
 	SDL_Rect dstRect;
 	float scaleX = 1;
@@ -24,7 +26,7 @@ private:
 	float timeElapsed = 0;
 	int width = 0;
 	int height = 0;
-	int destroyTexture;
+	int destroyTexture = 0;
 	SDL_RendererFlip flipHorizontal = SDL_FLIP_NONE;
 
 public:
@@ -33,7 +35,7 @@ public:
 	Sprite(SDL_Texture *tex, int frameCount = 1, float frameTime = 0, bool enableAlpha = false, bool loops = true);
 	~Sprite();
 	void Open(std::string file, bool enableAlpha=false);
-	void Update(float dt);
+	void Update(float dt = 0);
 	void Render(int x, int y, float angle = 0);
 	void Render(Vec2 pos, float angle = 0);
 	void SetClip(int x, int y, int w, int h);
@@ -41,13 +43,13 @@ public:
 	void SetScaleX(float scale);
 	void SetScaleY(float scale);
 	void SetTransparency(float a);
-	void SetFrame(int frame);
+	void SetFrame(int frame = 0);
 	void SetFrameNormalized(float f);
-	void SetFrameCount(int frameCount);
-	void SetFrameTime(float frameTime);
-	void SetBlending(bool b);
+	void SetFrameCount(int frameCount = 0);
+	void SetFrameTime(float frameTime = 0);
+	void SetBlending(bool b = false);
 	void SetTexture(SDL_Texture *tex, bool destroyTexture_=true);
-	void Mirror(bool m);
+	void Mirror(bool m = 0);
 	int GetWidth();
 	int GetHeight();
 	Vec2 GetSize();
