ab1e346ae582a93e6944e5cd08c9b76212029910
Thiago Ribeiro <thiagoflames@gmail.com>
2017-10-24 10:58:32 -0200

    Aplicação 3 das técnicas de inicializar variáveis e as de declarar o mais tarde possível em MapAlgorithm

--- src/MapAlgorithm.cpp
+++ src/MapAlgorithm.cpp
@@ -1,43 +1,58 @@
 #include "MapAlgorithm.h"
 #include <iostream>
 
+#include <assert.h>
+
 MapAlgorithm::MapAlgorithm(int numberOfRooms){
 	this->numberOfRooms = numberOfRooms;
 }
 
-void MapAlgorithm::RandomizeRoomOrder(std::vector<int>* roomOrder){
-	int random;
-
+void MapAlgorithm::RandomizeRoomOrder(std::vector<int>* roomOrder) {
 	srand(time(NULL));
 
-	for(int i = 0; i < numberOfRooms; i++){
+    int random = 0;
+
+    for(int i = 0; i < numberOfRooms; i++){
 		random = rand() % 3 + 2;
 		if(i == 0){
 			roomOrder->emplace_back(0);
 		}
-		else if(i == 5){
+		else if (i == 5){
 			roomOrder->emplace_back(1);
 		}
-		else{
+		else {
 			roomOrder->emplace_back(random);
 		}
+        assert(random >= INT_MIN_SIZE && random <= INT_MAX_SIZE);
 	}
 }
 
 bool MapAlgorithm::CanGo(int** roomArray, std::pair<int, int> pos, int sizeX, int sizeY){
-	if(pos.first < 0 || pos.second < 0){
+    assert(sizeX >= INT_MIN_SIZE && sizeX <= INT_MAX_SIZE);
+    assert(sizeY >= INT_MIN_SIZE && sizeY <= INT_MAX_SIZE);
+
+	if (pos.first < 0 || pos.second < 0) {
 		return false;
-	}
-	if(pos.first >= sizeX || pos.second >= sizeY){
+	} else {
+        // It does nothing.
+    }
+	if (pos.first >= sizeX || pos.second >= sizeY) {
 		return false;
-	}
+	} else {
+        // It does nothing.
+    }
 	if(roomArray[pos.first][pos.second] != -1){
 		return false;
-	}
+	} else {
+        // It does nothing.
+    }
 	return true;
 }
 
-bool MapAlgorithm::IsBlocked(int** roomArray, std::pair<int, int> pos, int sizeX, int sizeY){
+bool MapAlgorithm::IsBlocked(int** roomArray, std::pair<int, int> pos, int sizeX, int sizeY) {
+    assert(sizeX >= INT_MIN_SIZE && sizeX <= INT_MAX_SIZE);
+    assert(sizeY >= INT_MIN_SIZE && sizeY <= INT_MAX_SIZE);
+
 	bool t = false;
 	t = t || CanGo(roomArray, std::make_pair(pos.first + 1, pos.second), sizeX, sizeY);
 	t = t || CanGo(roomArray, std::make_pair(pos.first - 1, pos.second), sizeX, sizeY);
@@ -51,9 +66,12 @@ bool MapAlgorithm::IsBlocked(int** roomArray, std::pair<int, int> pos, int sizeX
 	t = t || IsBlocked()
 }*/
 
-void MapAlgorithm::CleanMap(int** roomArray, std::pair<int, int>* aux, int sizeX, int sizeY){
-	for(int i = 0; i < sizeX; i++){
-		for(int j = 0; j < sizeY; j++){
+void MapAlgorithm::CleanMap(int** roomArray, std::pair<int, int>* aux, int sizeX, int sizeY) {
+    assert(sizeX >= INT_MIN_SIZE && sizeX <= INT_MAX_SIZE);
+    assert(sizeY >= INT_MIN_SIZE && sizeY <= INT_MAX_SIZE);
+
+	for (int i = 0; i < sizeX; i++) {
+		for(int j = 0; j < sizeY; j++) {
 			roomArray[i][j] = -1;
 		}
 	}
@@ -62,55 +80,61 @@ void MapAlgorithm::CleanMap(int** roomArray, std::pair<int, int>* aux, int sizeX
 }
 
 void MapAlgorithm::PopulateRoomArray(int** roomArray, std::vector<int>* roomOrder, std::pair<int, int>* aux, /*std::vector<std::pair<int, int>>* roomWay,*/ int sizeX, int sizeY){
-	int random;
-	bool endRandom;
-
 	srand(time(NULL));
 
-	for(int i = 0; i < roomOrder->size(); i++){
+    int random = 0;
+    bool endRandom = false;
+	for (int i = 0; i < roomOrder->size(); i++) {
 		endRandom = false;
-		if(i == 0){
+		if (i == 0) {
 			roomArray[aux->first][aux->second] = roomOrder->at(i);
-		}
-		else{
-			if(IsBlocked(roomArray, std::make_pair(aux->first, aux->second), sizeX, sizeY)){
+		} else {
+			if (IsBlocked(roomArray, std::make_pair(aux->first, aux->second), sizeX, sizeY)) {
 				CleanMap(roomArray, aux, sizeX, sizeY);
 				this->PopulateRoomArray(roomArray, roomOrder, aux, sizeX, sizeY);
 				break;
 			}
-			while(endRandom == false){
+			while (endRandom == false) {
 				random = rand() % 4;
-				if(random == 0 && aux->first + 1 < sizeX){
-					if(roomArray[aux->first + 1][aux->second] == -1){
+				if (random == 0 && aux->first + 1 < sizeX) {
+					if (roomArray[aux->first + 1][aux->second] == -1) {
 						aux->first += 1;
 						roomArray[aux->first][aux->second] = roomOrder->at(i);
 						//roomWay->emplace_back(aux);
 						endRandom = true;
-					}
+					} else {
+                        // It does nothing.
+                    }
 				}
-				else if(random == 1 && aux->first - 1 > 0){
-					if(roomArray[aux->first - 1][aux->second] == -1){
+				else if (random == 1 && aux->first - 1 > 0){
+					if (roomArray[aux->first - 1][aux->second] == -1){
 						aux->first -= 1;
 						roomArray[aux->first][aux->second] = roomOrder->at(i);
 						//roomWay->emplace_back(aux);
 						endRandom = true;
-					}
+					} else {
+                        // It does nothing.
+                    }
 				}
-				else if(random == 2 && aux->second + 1 < sizeY){
-					if(roomArray[aux->first][aux->second + 1] == -1){
+				else if (random == 2 && aux->second + 1 < sizeY) {
+					if (roomArray[aux->first][aux->second + 1] == -1) {
 						aux->second += 1;
 						roomArray[aux->first][aux->second] = roomOrder->at(i);
 						//roomWay->emplace_back(aux);
 						endRandom = true;
-					}
+					} else {
+                        // It does nothing.
+                    }
 				}
-				else if(random == 3 && aux->second - 1 > 0){
-					if(roomArray[aux->first][aux->second - 1] == -1){
+				else if (random == 3 && aux->second - 1 > 0) {
+					if (roomArray[aux->first][aux->second - 1] == -1) {
 						aux->second -= 1;
 						roomArray[aux->first][aux->second] = roomOrder->at(i);
 						//roomWay->emplace_back(aux);
 						endRandom = true;
-					}
+					} else {
+                        // It does nothing
+                    }
 				}
 			}
 		}
