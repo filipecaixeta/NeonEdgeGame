d7187ee8e41185045fa4cd8b481ddf77eba39dd7
ulyssesBML <ulysses3353@gmail.com>
2017-10-31 19:15:17 -0200

    Aplicando verifique todo os valores de retorno 4

--- src/TileSet.cpp
+++ src/TileSet.cpp
@@ -20,33 +20,33 @@
  * @return: a instance of TileSet.
  */
 TileSet::TileSet(int tileWidth, int tileHeight, std::string file, int adjustX, int adjustY) {
-	//Log::instance.info("Initing TileSet");
-	if (tileWidth >= INT_MIN_SIZE && tileWidth <= INT_MAX_SIZE ) {
-		Log::instance.info("Seting tile columns");
+    //Log::instance.info("Initing TileSet");
+    if (tileWidth >= INT_MIN_SIZE && tileWidth <= INT_MAX_SIZE ) {
+        Log::instance.info("Seting tile columns");
         tileSet = Sprite(file); // Sprite file.
         TileSet::tileWidth = tileWidth; // Width of TileSet.
         columns = tileSet.GetWidth()/tileWidth; // Number of columns in TileSet.
     } else {
-		Log::instance.warning("Warning the tileWidth is not in the allowed range.");
+        Log::instance.warning("Warning the tileWidth is not in the allowed range.");
     }
     if (tileHeight >= INT_MIN_SIZE && tileHeight <= INT_MAX_SIZE) {
-		Log::instance.info("Seting tile rows");
+        Log::instance.info("Seting tile rows");
         TileSet::tileHeight = tileHeight; // Height of TileSet.
         rows = tileSet.GetHeight()/tileHeight; // Number of rows in TileSet.
     } else {
-		Log::instance.warning("Warning the tileHeight is not in the allowed range.");
+        Log::instance.warning("Warning the tileHeight is not in the allowed range.");
     }
     if(adjustX >= INT_MIN_SIZE && adjustX <= INT_MAX_SIZE){
-		Log::instance.info("Seting tile adjustX");
+        Log::instance.info("Seting tile adjustX");
         TileSet::adjustX = adjustX; // Adjust X axis.
     } else {
-		Log::instance.warning("Warning the adjustX is not in the allowed range.");
+        Log::instance.warning("Warning the adjustX is not in the allowed range.");
     }
     if(adjustY >= INT_MIN_SIZE && adjustY <= INT_MAX_SIZE){
-		Log::instance.info("Seting tile adjustY");
+        Log::instance.info("Seting tile adjustY");
         TileSet::adjustY = adjustY; // Ajdust Y axis.
     } else {
-		Log::instance.warning("Warning the adjustY is not in the allowed range.");
+        Log::instance.warning("Warning the adjustY is not in the allowed range.");
     }
 }
 
@@ -58,7 +58,7 @@ TileSet::TileSet(int tileWidth, int tileHeight, std::string file, int adjustX, i
  * @return: none.
  */
 TileSet::~TileSet() {
-	Log::instance.info("Destroy TileSet");
+    Log::instance.info("Destroy TileSet");
 }
 
 /**
@@ -68,22 +68,22 @@ TileSet::~TileSet() {
  * @param int x -.
  * @param int y -.
  * @return: none.
-*/
+ */
 void TileSet::Render(int index, int x, int y) {
-	if (index >= INT_MIN_SIZE && index <= INT_MAX_SIZE) {
+    if (index >= INT_MIN_SIZE && index <= INT_MAX_SIZE) {
         if ((x >= INT_MIN_SIZE && x <= INT_MAX_SIZE) && (y >= INT_MIN_SIZE && y <= INT_MAX_SIZE)) {
             // Valid if index is between 0 and end of matrix
             if(index > -1 && index < columns*rows) {
                 tileSet.SetClip(index%columns*(tileWidth+adjustX), index/columns*(tileHeight+adjustY),
-                 tileWidth+adjustX, tileHeight+adjustY);
+                                tileWidth+adjustX, tileHeight+adjustY);
                 tileSet.Render(x-adjustX, y-adjustY);
             } else {
             }
         } else {
-			Log::instance.warning("tileSet x value out of range");
+            Log::instance.warning("tileSet x value out of range");
         }
     } else{
-		Log::instance.warning("tileSet index out of range");
+        Log::instance.warning("tileSet index out of range");
     }
 }
 
@@ -95,7 +95,11 @@ void TileSet::Render(int index, int x, int y) {
  * @return: sprite of TileSet.
  */
 Sprite TileSet::GetTileSet() {
-    return tileSet;
+    if(&tileSet != NULL){
+        return tileSet;
+    } else {
+        Log::instance.warning("Width of tileset is low than 0.");
+    }
 }
 
 /**
@@ -104,9 +108,13 @@ Sprite TileSet::GetTileSet() {
  * @param none.
  * @param none.
  * @return: int tileWidth.
-*/
+ */
 int TileSet::GetTileWidth() {
-    return tileWidth;
+    if(tileWidth > 0){
+        return tileWidth;
+    } else{
+        Log::instance.warning("Width of tileset is low than 0.");
+    }
 }
 
 /**
@@ -115,9 +123,13 @@ int TileSet::GetTileWidth() {
  * @param none.
  * @param none.
  * @return: int tileHeight.
-*/
+ */
 int TileSet::GetTileHeight() {
-    return tileHeight;
+    if(tileHeight > 0){
+        return tileHeight;
+    } else{
+        Log::instance.warning("Height of tileset is low than 0.");
+    }
 }
 
 /**
@@ -126,9 +138,14 @@ int TileSet::GetTileHeight() {
  * @param none.
  * @param none.
  * @return: int columns.
-*/
+ */
 int TileSet::GetColumns() {
-    return columns;
+
+    if(rows > 0){
+        return columns;
+    } else {
+        Log::instance.warning("columns of tileset is low than 0.");
+    }
 }
 
 /**
@@ -139,5 +156,9 @@ int TileSet::GetColumns() {
 
 */
 int TileSet::GetRows() {
-    return rows;
+    if(rows > 0){
+        return rows;
+    } else {
+        Log::instance.warning("rown of tileset is low than 0.");
+    }
 }
